7a3f81fe998abe9923f91280b534a1829cc7689f
Florian Rappl <florian.rappl@smapiot.com>
2020-10-13 00:08:33 +0200

    Finished adoption

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css)$': 'identity-obj-proxy',
     '@openmrs/esm-api': '<rootDir>/__mocks__/openmrs-esm-api.mock.tsx',
-    '@openmrs/esm-module-config': '<rootDir>/__mocks__/openmrs-esm-module-config.mock.tsx',
+    '@openmrs/esm-config': '<rootDir>/__mocks__/openmrs-esm-module-config.mock.tsx',
     '@openmrs/esm-error-handling': '<rootDir>/__mocks__/openmrs-esm-error-handling.mock.tsx',
     '@openmrs/esm-styleguide': '<rootDir>/__mocks__/openmrs-esm-styleguide.mock.tsx',
     'react-i18next': '<rootDir>/__mocks__/react-i18next.js',
