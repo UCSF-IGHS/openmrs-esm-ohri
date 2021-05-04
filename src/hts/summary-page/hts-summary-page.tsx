fad63d63e588c1a619909951d58abd155b910e55
samuelmale <samuelsmalek@gmail.com>
2021-05-04 09:48:48 +0300

    Fix typecript errors and apply linter

--- src/hts/summary-page/hts-summary-page.tsx
+++ src/hts/summary-page/hts-summary-page.tsx
@@ -2,10 +2,7 @@ import { ExtensionSlot } from '@openmrs/esm-framework';
 import * as React from 'react';
 
 function HtsSummaryPage() {
-    console.log('HTS SUMMARY PAGE LOADED')
-    return (
-       <div>This is the hts summary page</div>
-    );
+  return <div>This is the hts summary page</div>;
 }
 
 export default HtsSummaryPage;
