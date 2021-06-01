b29733c2e1e2d066c8e5bd2eb0142e8fdaf67a7b
Lars Albino Lemos <lars.lemos@ucsf.edu>
2021-06-01 19:06:33 +0200

    Revert "Resolving differences and merging"
    This reverts commit 1f96119082f12baa8c1aa124719b20489cba553d, reversing
    changes made to c80ae82d10a2a2c6a27f8806cc37e025bcdddb64.

--- src/forms/submission-handlers/base-handlers.test.ts
+++ src/forms/submission-handlers/base-handlers.test.ts
@@ -1,14 +1,3 @@
 describe('ObsSubmissionHandler', () => {
   it('get initial value', () => {});
 });
-
-// import { getValue } from './base-handlers';
-
-// describe('obs-handler', () => {
-//   describe.skip('getValues', () => {
-//     it('uses the context and value to get values', async () => {
-//       const result = await getValue();
-//       expect(result).toEqual([{}]);
-//     });
-//   });
-// });
