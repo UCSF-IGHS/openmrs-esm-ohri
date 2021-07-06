4b011b31adf5760ce382a4572273222ff75fe81f
Samuel Male <samuelsmalek@gmail.com>
2021-07-06 14:27:52 +0300

    OHRI-120: Implement pagination

--- src/hts/patient-list/patient-list.component.tsx
+++ src/hts/patient-list/patient-list.component.tsx
@@ -1,38 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './patient-list.scss';
 import Button from 'carbon-components-react/es/components/Button';
 import { Add16 } from '@carbon/icons-react';
 import { useTranslation } from 'react-i18next';
 import OTable from '../../components/data-table/o-table.component';
-import { attach, openmrsFetch, switchTo, age, navigate, usePagination } from '@openmrs/esm-framework';
-import { DataTableSkeleton, Pagination } from 'carbon-components-react';
+import { openmrsFetch, age, navigate } from '@openmrs/esm-framework';
+import { DataTableSkeleton, Link, Pagination } from 'carbon-components-react';
 import EmptyState from '../../components/empty-state/empty-state.component';
-import moment from 'moment';
 import { capitalize } from 'lodash';
-import { paginate } from './pagination';
-import dayjs from 'dayjs';
 
 interface HtsOverviewListProps {
   patientUuid: string;
 }
 
-const PatientList: React.FC<HtsOverviewListProps> = ({ patientUuid }) => {
+const PatientList: React.FC<HtsOverviewListProps> = () => {
   const { t } = useTranslation();
   const [patients, setTableRows] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [counter, setCounter] = useState(0);
   const rowCount = 5;
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
-  const [currentPatientPage] = paginate(patients, page, pageSize);
-  const [currentPage, setCurrentPage] = useState(0);
-  const pagination = usePagination(patients, pageSize);
   const [totalPatientCount, setPatientCount] = useState(0);
-
-  const forceComponentUpdate = () => setCounter(counter + 1);
-  const addNewPatient = () => navigate({ to: '${openmrsSpaBase}/patient-registration' });
-
+  const [nextOffSet, setNextOffSet] = useState(0);
+  const headerTitle = 'Patient List';
   const tableHeaders = [
     { key: 'name', header: 'Name', isSortable: true },
     { key: 'gender', header: 'Gender' },
@@ -40,34 +31,40 @@ const PatientList: React.FC<HtsOverviewListProps> = ({ patientUuid }) => {
     { key: 'last_visit', header: 'Last Visit' },
   ];
 
-  function getPatients() {
-    return openmrsFetch(`/ws/fhir2/R4/Patient`).then(({ data }) => {
+  useEffect(() => {
+    setIsLoading(true);
+    loadPatients(nextOffSet, pageSize);
+  }, [page, pageSize]);
+
+  const addNewPatient = () => navigate({ to: '${openmrsSpaBase}/patient-registration' });
+  const navigateToPatientDB = patientUuid => navigate({ to: '${openmrsSpaBase}/patient/' + `${patientUuid}/chart` });
+
+  function loadPatients(offSet: number, pageSize: number) {
+    return openmrsFetch(`/ws/fhir2/R4/Patient?_getpagesoffset=${offSet}&_count=${pageSize}`).then(({ data }) => {
       let rows = [];
+      setPatientCount(data.total);
       data.entry.forEach(patient => {
         rows.push({
           id: patient.resource.id,
-          name: `${patient.resource.name[0].given.join(' ')} ${patient.resource.name[0].family}`,
+          name: (
+            <Link
+              onClick={e => {
+                e.preventDefault();
+                navigateToPatientDB(patient.resource.id);
+              }}>
+              {`${patient.resource.name[0].given.join(' ')} ${patient.resource.name[0].family}`}
+            </Link>
+          ),
           gender: capitalize(patient.resource.gender),
           age: age(patient.resource.birthDate),
           // last_visit: moment(patient.encounterDatetime).format('DD-MMM-YYYY'),
           last_visit: '--',
-          patientUrl: patient.fullUrl,
         });
       });
-
       setTableRows(rows);
       setIsLoading(false);
     });
   }
-  function handlePageChange(index: number) {
-    setCurrentPage(index);
-  }
-  React.useEffect(() => {
-    getPatients();
-  }, [counter]);
-
-  const headerTitle = 'Patient List';
-
   return (
     <>
       {isLoading ? (
@@ -95,8 +92,9 @@ const PatientList: React.FC<HtsOverviewListProps> = ({ patientUuid }) => {
               page={page}
               pageSize={pageSize}
               pageSizes={[10, 20, 30, 40, 50]}
-              totalItems={patients.length}
+              totalItems={totalPatientCount}
               onChange={({ page, pageSize }) => {
+                setNextOffSet(page * pageSize + 1);
                 setPage(page);
                 setPageSize(pageSize);
               }}
