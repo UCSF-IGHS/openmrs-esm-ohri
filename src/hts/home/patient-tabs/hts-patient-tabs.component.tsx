c348bd4f5c4dd567bf829aa902f1fdac6d9cc12d
herbertug <herbertoketayot@gmail.com>
2021-07-13 09:04:22 +0300

    clean up header slot

--- src/hts/home/patient-tabs/hts-patient-tabs.component.tsx
+++ src/hts/home/patient-tabs/hts-patient-tabs.component.tsx
@@ -3,19 +3,20 @@ import { Tabs, Tab } from 'carbon-components-react';
 
 function HTSPatientTabs() {
   return (
-    <div>
-      <Tabs scrollIntoView={false} type="container">
-        <Tab href="#" id="tab-1" label="Tab label 1">
-          <div className="some-content">Content for first tab goes here.</div>
-        </Tab>
-        <Tab href="#" id="tab-1" label="Tab label 1">
-          <div className="some-content">Content for second tab goes here.</div>
-        </Tab>
-        <Tab href="#" id="tab-1" label="Tab label 1">
-          <div className="some-content">Content for third tab goes here.</div>
-        </Tab>
-      </Tabs>
-    </div>
+    <Tabs type="container">
+      <Tab id="tab-1" label="Tab label 1">
+        <p>Content for first tab goes here.</p>
+      </Tab>
+      <Tab id="tab-2" label="Tab label 2">
+        <p>Content for second tab goes here.</p>
+      </Tab>
+      <Tab id="tab-3" label="Tab label 3 shows truncation" title="Tab label 3 shows truncation">
+        <p>Content for third tab goes here.</p>
+      </Tab>
+      <Tab label={<div>Custom Label</div>}>
+        <p>Content for fourth tab goes here.</p>
+      </Tab>
+    </Tabs>
   );
 }
 
---
+++ src/hts/home/summary-tile/hts-summary-tile.component.tsx
@@ -0,0 +1,42 @@
+import { Column, Row } from 'carbon-components-react';
+import React, { useState } from 'react';
+import OHRISummaryTile from '../../../components/tile/ohri-summary-tile.component';
+
+function HTSSummaryTile() {
+  // const [tiles, setTiles] = useState(['Today’s clients', 'Positive in last 14 days', 'Linked to care in last 14 days'])
+
+  const tile = [
+    {
+      title: 'Test',
+      linkAddres: '#',
+      subTitle: 'Test Sub',
+      value: 45,
+    },
+    {
+      title: 'Test 2',
+      linkAddres: '#',
+      subTitle: 'Test Sub',
+      value: 46,
+    },
+    {
+      title: 'Test 3',
+      linkAddres: '#',
+      subTitle: 'Test Sub',
+      value: 43,
+    },
+  ];
+
+  return (
+    <Row>
+      {tile.map((name, index) => {
+        return (
+          <Column lg={4} md={3} sm={2} key={index}>
+            <OHRISummaryTile details={name} />
+          </Column>
+        );
+      })}
+    </Row>
+  );
+}
+
+export default HTSSummaryTile;
