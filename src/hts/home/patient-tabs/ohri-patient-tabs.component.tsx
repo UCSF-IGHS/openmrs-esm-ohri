43c016d0d150bcc1129faad59169a50cd682206f
herbertug <herbertoketayot@gmail.com>
2021-07-15 08:35:20 +0300

    base implementation for tablet tile

--- src/hts/home/patient-tabs/ohri-patient-tabs.component.tsx
+++ src/hts/home/patient-tabs/ohri-patient-tabs.component.tsx
@@ -6,17 +6,14 @@ function OHRIPatientTabs() {
   return (
     <Row className={styles.container}>
       <Tabs type="container">
-        <Tab className={styles.tabContainer} id="tab-1" label="Waiting for pre-test counselling">
-          <p>Content for first tab goes here.</p>
+        <Tab id="tab-1" label="Waiting for pre-test counselling">
+          <Row className={styles.tabContent}>Content for second tab goes here.</Row>
         </Tab>
-        <Tab className={styles.tabContainer} id="tab-2" label="Waiting for HIV test">
-          <p>Content for second tab goes here.</p>
+        <Tab id="tab-2" label="Waiting for HIV test" className={styles.tabContent}>
+          <div>Content for second tab goes here.</div>
         </Tab>
-        <Tab id="tab-3" label="Waiting for post-test counselling" title="Tab label 3 shows truncation">
-          <p>Content for third tab goes here.</p>
-        </Tab>
-        <Tab className={styles.customTab} label={<div className={styles.customTabContent}>View all lists</div>}>
-          <p>Content for fourth tab goes here.</p>
+        <Tab id="tab-3" label="Waiting for post-test counselling" className={styles.tabContent}>
+          <div className={styles.tabContent}>Content for second tab goes here.</div>
         </Tab>
       </Tabs>
     </Row>
--- src/hts/home/welcome-section/hts-welcome-section.component.tsx
+++ src/hts/home/welcome-section/hts-welcome-section.component.tsx
@@ -3,27 +3,17 @@ import styles from './hts-welcome-section.scss';
 import { Calendar32 } from '@carbon/icons-react';
 import { Column, Row } from 'carbon-components-react';
 
-function HtsWelcomeSection({ tile }) {
+function HtsWelcomeSection() {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.welcome}>OHRI Health Clinic</div>
+      <div className={styles.welcome}>OHRI Health Clinic</div>
       <div className={styles.welcomeDetails}>
         <div className={styles.userWelcome}>Welcome back, username</div>
         <div className={styles.currentDate}>
-          <Calendar32 />
-          16 June 2021
-        </div>
-      </div> */}
-      <Row className={styles.welcome}>OHRI Health Clinic</Row>
-      <Row className={styles.welcomeDetails}>
-        <Column lg={6} md={6} sm={2} className={styles.userWelcome}>
-          Welcome back, username
-        </Column>
-        <Column lg={6} md={6} sm={2} className={styles.currentDate}>
           <Calendar32 className={styles.calendarIcon} />
           16 June 2021
-        </Column>
-      </Row>
+        </div>
+      </div>
     </div>
   );
 }
