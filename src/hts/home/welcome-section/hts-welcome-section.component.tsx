c4c22bcf595158a8ac6397382f61812746a90032
herbertug <herbertoketayot@gmail.com>
2021-07-15 10:55:24 +0300

    add current date

--- src/hts/home/welcome-section/hts-welcome-section.component.tsx
+++ src/hts/home/welcome-section/hts-welcome-section.component.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './hts-welcome-section.scss';
 import { Calendar32 } from '@carbon/icons-react';
-import { Column, Row } from 'carbon-components-react';
+import moment from 'moment';
 
 function HtsWelcomeSection() {
   return (
     <div className={styles.container}>
       <div className={styles.welcome}>OHRI Health Clinic</div>
       <div className={styles.welcomeDetails}>
-        <div className={styles.userWelcome}>Welcome back, username</div>
+        <span className={styles.userWelcome}>Welcome back, username</span>
         <div className={styles.currentDate}>
           <Calendar32 className={styles.calendarIcon} />
-          16 June 2021
+          {/* 16 June 2021 */}
+          {moment().format('DD MMMM YYYY')}
         </div>
       </div>
     </div>
