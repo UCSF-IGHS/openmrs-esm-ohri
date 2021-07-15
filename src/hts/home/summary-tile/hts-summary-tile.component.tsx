dd2fbc1840a2f84fad13d5a03a061c48d70f259c
pirupius <piruville@gmail.com>
2021-07-15 17:51:07 +0300

    ohri-148 api enhancements and code clean up

--- src/hts/home/summary-tile/hts-summary-tile.component.tsx
+++ src/hts/home/summary-tile/hts-summary-tile.component.tsx
@@ -1,18 +1,18 @@
 import { Column, Row } from 'carbon-components-react';
 import React, { useEffect, useState } from 'react';
 import OHRISummaryTile from '../../../components/tile/ohri-summary-tile.component';
+import { fetchTodayClients, fetchObservationsFromCodeConcept } from '../../../api/api';
 import {
-  fetchPatientsLinkedToCareInLast14Days,
-  fetchPositivePatientsInLast14Days,
-  fetchTodayClients,
-} from '../../../api/api';
+  finalHIVCodeConcept,
+  finalPositiveHIVValueConcept,
+  linkedToCareCodeConcept,
+  linkedToCareYesValueConcept,
+} from '../../../constants';
 
 function HTSSummaryTile() {
   const [todayPatientCount, setTodayPatientCount] = useState(0);
   const [positiveInLast14Days, setPositiveInLast14Days] = useState(0);
   const [linkedToCareInLast14Days, setLinkedToCareInLast14Days] = useState(0);
-  const finalHIVCodeConcept = '6378487b-584d-4422-a6a6-56c8830873ff'; // dev
-  const linkageToCareConcept = '1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'; // dev
 
   const tile = [
     {
@@ -48,23 +48,24 @@ function HTSSummaryTile() {
   }
 
   function getPositiveInLast14days() {
-    return fetchPositivePatientsInLast14Days(finalHIVCodeConcept).then(({ data }) => {
+    return fetchObservationsFromCodeConcept(finalHIVCodeConcept, finalPositiveHIVValueConcept, 14).then(({ data }) => {
       setPositiveInLast14Days(data.total);
     });
   }
 
   function getLinkedToCareInLast14days() {
-    return fetchPatientsLinkedToCareInLast14Days(linkageToCareConcept).then(({ data }) => {
-      setLinkedToCareInLast14Days(data.total);
-    });
+    return fetchObservationsFromCodeConcept(linkedToCareCodeConcept, linkedToCareYesValueConcept, 14).then(
+      ({ data }) => {
+        setLinkedToCareInLast14Days(data.total);
+      },
+    );
   }
 
   return (
     <Row>
       {tile.map((name, index) => {
         return (
-          // define a col
-          <Column lg={4} md={3} sm={2}>
+          <Column lg={4} md={3} sm={1} key={index}>
             <OHRISummaryTile details={name} />
           </Column>
         );
