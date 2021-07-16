d0bae891185a33f80a1d38cbf2fa657b0b9d746f
Lars Albino Lemos <lars.lemos@ucsf.edu>
2021-07-16 15:11:28 +0200

    ohri-159-navigation, dasboard, lint fix

--- src/hts/client-linkage/client-linkage-form-section.component.tsx
+++ src/hts/client-linkage/client-linkage-form-section.component.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
 import styles from '../encounters-list/hts-overview-list.scss';
 import { useTranslation } from 'react-i18next';
 import EmptyState from '../../components/empty-state/empty-state-boilerplate.component';
@@ -9,14 +9,21 @@ const ClientLinkage = () => {
   const [rows, setRows] = useState([]);
 
   return (
-      <> 
-          <div className={styles.widgetContainer} style={{ marginTop: '2.5rem' }}>
-            <div className={styles.widgetHeaderContainer}>
-              <h4 className={`${styles.productiveHeading03} ${styles.text02}`}>{headerTitle}</h4>
-            </div>
+    <>
+      {rows.length ? (
+        <div className={styles.widgetContainer} style={{ marginTop: '2.5rem' }}>
+          <div className={styles.widgetHeaderContainer}>
+            <h4 className={`${styles.productiveHeading03} ${styles.text02}`}>{headerTitle}</h4>
           </div>
-
-      </>
+        </div>
+      ) : (
+        <EmptyState
+          displayText={t('clientLinkage', 'client linkage')}
+          headerTitle={headerTitle}
+          launchForm={() => {}}
+        />
+      )}
+    </>
   );
 };
 
