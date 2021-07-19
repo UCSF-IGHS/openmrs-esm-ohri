80b5760dc45f413246b26eccf6c23f634880b9af
herbertug <herbertoketayot@gmail.com>
2021-07-19 13:37:16 +0300

    UI clean up

--- src/dashboard.meta.tsx
+++ src/dashboard.meta.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { ConfigurableLink } from '@openmrs/esm-framework';
 import { SideNav, SideNavMenu, SideNavMenuItem } from 'carbon-components-react';
+import styles from './dashboard.scss';
 
 export const createDashboardLink = db => {
   const DashboardLink: React.FC<{ basePath: string }> = ({ basePath }) => {
     return (
-<<<<<<< HEAD
-      // <div key={db.name}>
-      //   <ConfigurableLink to={`${basePath}/${db.name}`} className="bx--side-nav__link">
-      //     {db.title}
-      //   </ConfigurableLink>
-      // </div>
-      <SideNavMenu title="HIV">
-        <SideNavMenuItem href={`${basePath}/${db.name}`}> HTS </SideNavMenuItem>
-=======
       <SideNavMenu title="HIV" className={styles.noMarker} defaultExpanded={true}>
         <SideNavMenuItem isActive href={`${basePath}/${db.name}`}>
           {' '}
           HTS{' '}
         </SideNavMenuItem>
->>>>>>> d0bae89 (ohri-159-navigation, dasboard, lint fix)
         <SideNavMenuItem> Care and Treatment </SideNavMenuItem>
         <SideNavMenuItem> PMTCT </SideNavMenuItem>
       </SideNavMenu>
