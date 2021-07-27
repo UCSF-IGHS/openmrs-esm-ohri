bed7c1ab91c935c5a9fa63767ee3d04e3b92dcf8
herbertug <herbertoketayot@gmail.com>
2021-07-27 06:17:03 +0300

    handle parent overflow on scroll

--- src/forms/components/sidebar/ohri-form-sidebar.component.tsx
+++ src/forms/components/sidebar/ohri-form-sidebar.component.tsx
@@ -39,42 +39,6 @@ function OHRIFormSidebar({ currentPage, selectedPage, mode, onCancel }) {
         {mode == 'view' ? 'Close' : 'Cancel'}
       </Button>
     </div>
-    // <>
-    //   <SideNav
-    //     isFixedNav
-    //     expanded={true}
-    //     isChildOfHeader={false}
-    //     aria-label="Side navigation"
-    //     style={{ top: '8.5rem', width: '12.6875rem', marginLeft: '9px' }}
-    //   >
-    //     <SideNavItems>
-    //       {currentPage.map((page, index) => {
-    //         return (
-    //           <SideNavLink
-    //             onClick={() => handleClick(page.label)}
-    //             className={joinWord(page.label) === selectedPage ? styles.sidebarSectionActive : styles.sidebarSection}
-    //             key={index}>
-    //             {page.label}
-    //           </SideNavLink>
-    //         );
-    //       })}
-    //       <hr className={styles.sideBarHorizontalLine} />
-    //       {mode != 'view' && (
-    //         <Button
-    //           style={{ marginBottom: '0.625rem', width: '11.688rem', display: 'block' }}
-    //           type="submit">
-    //           Save
-    //         </Button>
-    //       )}
-    //       <Button
-    //         style={{ width: '11.688rem' }}
-    //         kind="tertiary"
-    //         onClick={() => (onCancel ? onCancel() : null)}>
-    //         {mode == 'view' ? 'Close' : 'Cancel'}
-    //       </Button>
-    //     </SideNavItems>
-    //   </SideNav>
-    // </>
   );
 }
 
