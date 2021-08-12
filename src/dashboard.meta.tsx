c23ee84808f755e3d8ec49fa8a07e8e52532530b
Lars Albino Lemos <lars.lemos@ucsf.edu>
2021-08-12 15:58:16 +0200

    ohri-178-care0treament-lin, page, list

--- src/dashboard.meta.tsx
+++ src/dashboard.meta.tsx
@@ -1,27 +1,20 @@
-import React from 'react';
-import { ConfigurableLink } from '@openmrs/esm-framework';
-import { SideNav, SideNavMenu, SideNavMenuItem } from 'carbon-components-react';
-import styles from './dashboard.scss';
-
-export const createDashboardLink = db => {
-  const DashboardLink: React.FC<{ basePath: string }> = ({ basePath }) => {
-    return (
-      <SideNavMenu title="HIV" className={styles.noMarker} defaultExpanded={true}>
-        <SideNavMenuItem className={styles.currentNavItem} href={`${basePath}/${db.name}`}>
-          {' '}
-          HTS{' '}
-        </SideNavMenuItem>
-        <SideNavMenuItem> Care and Treatment </SideNavMenuItem>
-        <SideNavMenuItem> PMTCT </SideNavMenuItem>
-      </SideNavMenu>
-    );
-  };
-  return DashboardLink;
-};
-
 export const dashboardMeta = {
-  name: 'hts-summary',
-  slot: 'hts-summary-dashboard-slot',
-  config: { columns: 1, type: 'grid' },
-  title: 'HTS Sessions',
+  hts: {
+    name: 'hts-summary',
+    slot: 'hts-summary-dashboard-slot',
+    config: { columns: 1, type: 'grid' },
+    title: 'HTS ',
+  },
+  caretreament: {
+    name: 'care-and-treatment',
+    slot: 'hts-summary-dashboard-slot',
+    config: { columns: 1, type: 'grid' },
+    title: 'Care and Treatment',
+  },
+  pmtct: {
+    name: 'PMTCT',
+    slot: 'hts-summary-dashboard-slot',
+    config: { columns: 1, type: 'grid' },
+    title: 'PMTCT',
+  },
 };
