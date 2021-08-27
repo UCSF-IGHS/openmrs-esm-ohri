6bccc28411dd4a0e624cff602de92bc691c08521
herbertug <herbertoketayot@gmail.com>
2021-08-27 03:14:24 +0300

    styling form to take up parent spacing

--- src/workspace/ohri-workspace.component.tsx
+++ src/workspace/ohri-workspace.component.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ExtensionSlot } from '@openmrs/esm-framework';
 import { workspaceContext, WorkspaceContextProps } from './ohri-workspace-utils';
+import styles from './patient-list-workspace.scss';
 
 const OHRIWorkspace: React.FC<{ closeWorkspace: () => {}; patientUuid: string; viewMode: string }> = ({
   closeWorkspace,
@@ -14,9 +15,10 @@ const OHRIWorkspace: React.FC<{ closeWorkspace: () => {}; patientUuid: string; v
   }, []);
 
   return (
-    <div>
+    <>
       {context && (
         <ExtensionSlot
+          className={styles.Extentioncontainer}
           extensionSlotName={OHRIWorkspaceSlot}
           state={{
             encounterUuid: context.encounterUuid,
@@ -29,7 +31,7 @@ const OHRIWorkspace: React.FC<{ closeWorkspace: () => {}; patientUuid: string; v
           }}
         />
       )}
-    </div>
+    </>
   );
 };
 
