d571d4749dddfe3859a1d363f0c2d33fe1a088b2
Samuel Male <samuelsmalek@gmail.com>
2021-09-07 10:18:28 +0300

    Add group obs files

---
+++ src/forms/components/group/ohri-obs-group.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { OHRIFormFieldProps } from '../../types';
+
+export const OHRIObsGroup: React.FC<OHRIFormFieldProps> = ({ question }) => {
+  // question['questions']
+  return <div></div>;
+};
