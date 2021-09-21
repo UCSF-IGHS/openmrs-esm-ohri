4a71e0035c9fcd533f9214086259d4d68e8ce949
pirupius <piruville@gmail.com>
2021-09-21 14:03:36 +0300

    OHRI-227 encounter list component

--- src/components/encounter-list/encounter-list.component.tsx
+++ src/components/encounter-list/encounter-list.component.tsx
@@ -1,26 +1,33 @@
 import { openmrsFetch } from '@openmrs/esm-framework';
 import DataTableSkeleton from 'carbon-components-react/lib/components/DataTableSkeleton';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import EmptyState from '../empty-state/empty-state.component';
 import { launchOHRIWorkSpace } from '../../workspace/ohri-workspace-utils';
-import { clinicalVisitEncounterType, encounterRepresentation } from '../../constants';
 import { getForm } from '../../utils/forms-loader';
 import { launchForm } from '../../utils/ohri-forms-commons';
 import { OHRIFormLauncherWithIntent } from '../ohri-form-launcher/ohri-form-laucher.componet';
 import styles from '../../hts/care-and-treatment/service-enrolment/service-enrolment-list.scss';
 import OTable from '../data-table/o-table.component';
-import { Button } from 'carbon-components-react';
+import { Button, OverflowMenu, OverflowMenuItem } from 'carbon-components-react';
 import { Add16 } from '@carbon/icons-react';
+import { clinicalVisitEncounterType, encounterRepresentation, visitTypeConcept } from '../../constants';
+import moment from 'moment';
 
-interface EncounterListProps {
+export interface EncounterListColumn {
+  conceptUuid?: string;
+  key: string;
+  header: string;
+  getValue: (encounter: any) => string;
+}
+
+export interface EncounterListProps {
   patientUuid: string;
   encounterUuid: string;
   form?: { package: string; name: string };
   columns: Array<any>;
   headerTitle: string;
-  tableHeaders: Array<string>;
-  displayText: string;
+  description: string;
 }
 
 const EncounterList: React.FC<EncounterListProps> = ({
@@ -29,14 +36,77 @@ const EncounterList: React.FC<EncounterListProps> = ({
   form,
   columns,
   headerTitle,
-  tableHeaders,
-  displayText,
+  description,
 }) => {
   const { t } = useTranslation();
   const [tableRows, setTableRows] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [counter, setCounter] = useState(0);
   const [encounterForm, setEncounterForm] = useState(getForm(form.package, form.name));
+
+  const editServiceEnrolmentEncounter = encounterUuid => {
+    launchOHRIWorkSpace('ohri-forms-view-ext', {
+      title: encounterForm.name,
+      screenSize: 'maximize',
+      encounterUuid: encounterUuid,
+      state: { updateParent: forceComponentUpdate, formJson: encounterForm },
+    });
+  };
+  const viewHTSEncounter = encounterUuid => {
+    launchOHRIWorkSpace('ohri-forms-view-ext', {
+      title: encounterForm.name,
+      screenSize: 'maximize',
+      encounterUuid: encounterUuid,
+      mode: 'view',
+      state: { updateParent: forceComponentUpdate, formJson: encounterForm },
+    });
+  };
+
+  const headers = useMemo(() => {
+    if (columns) {
+      return columns.map(column => {
+        return { key: column.key, header: column.header };
+      });
+    }
+    return [];
+  }, [columns]);
+
+  const loadRows = useCallback(
+    encounterType => {
+      const query = `encounterType=${encounterType}&patient=${patientUuid}`;
+
+      openmrsFetch(`/ws/rest/v1/encounter?${query}&v=${encounterRepresentation}`).then(({ data }) => {
+        const rows = data.results.map(encounter => {
+          const row = {};
+          columns.forEach(column => {
+            row[column.key] = column.getValue(encounter);
+          });
+          row['actions'] = (
+            <OverflowMenu flipped className={styles.flippedOverflowMenu}>
+              <OverflowMenuItem
+                itemText={t('viewHTSEncounter', 'View')}
+                onClick={e => {
+                  e.preventDefault();
+                  viewHTSEncounter(encounter.uuid);
+                }}
+              />
+              <OverflowMenuItem
+                itemText={t('editServiceEnrolmentEncounter', 'Edit')}
+                onClick={e => {
+                  e.preventDefault();
+                  editServiceEnrolmentEncounter(encounter.uuid);
+                }}
+              />
+            </OverflowMenu>
+          );
+          return row;
+        });
+        setTableRows(rows);
+        setIsLoading(false);
+      });
+    },
+    [patientUuid],
+  );
 
   const forceComponentUpdate = () => setCounter(counter + 1);
 
@@ -48,6 +118,10 @@ const EncounterList: React.FC<EncounterListProps> = ({
     });
   };
 
+  useEffect(() => {
+    loadRows(clinicalVisitEncounterType);
+  }, [loadRows]);
+
   return (
     <>
       {isLoading ? (
@@ -58,24 +132,19 @@ const EncounterList: React.FC<EncounterListProps> = ({
             <div className={styles.widgetHeaderContainer}>
               <h4 className={`${styles.productiveHeading03} ${styles.text02}`}>{headerTitle}</h4>
               <div className={styles.toggleButtons}>
-                <Button
-                  kind="ghost"
-                  renderIcon={Add16}
-                  iconDescription="New"
-                  onClick={e => {
-                    e.preventDefault();
-                    launchEncounterForm();
-                  }}>
-                  {t('Add')}
-                </Button>
+                <OHRIFormLauncherWithIntent
+                  formJson={encounterForm}
+                  launchForm={launchEncounterForm}
+                  onChangeIntent={encounterForm}
+                />
               </div>
             </div>
-            <OTable tableHeaders={tableHeaders} tableRows={tableRows} />
+            <OTable tableHeaders={headers} tableRows={tableRows} />
           </div>
         </>
       ) : (
         <EmptyState
-          displayText={displayText}
+          displayText={description}
           headerTitle={headerTitle}
           launchForm={launchEncounterForm}
           launchFormComponent={
--- src/pages/clinical-visit/encounter-list/clinical-visit-encounter-list.component.tsx
+++ src/pages/clinical-visit/encounter-list/clinical-visit-encounter-list.component.tsx
@@ -1,86 +1,78 @@
-import { openmrsFetch } from '@openmrs/esm-framework';
-import DataTableSkeleton from 'carbon-components-react/lib/components/DataTableSkeleton';
-import React, { useCallback, useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import EmptyState from '../../../components/empty-state/empty-state.component';
-import { launchOHRIWorkSpace } from '../../../workspace/ohri-workspace-utils';
-import { clinicalVisitEncounterType, encounterRepresentation } from '../../../constants';
-import { getForm } from '../../../utils/forms-loader';
-import { launchForm } from '../../../utils/ohri-forms-commons';
-import { OHRIFormLauncherWithIntent } from '../../../components/ohri-form-launcher/ohri-form-laucher.componet';
-import EncounterList from '../../../components/encounter-list/encounter-list.component';
+import React from 'react';
+import {
+  clinicalVisitEncounterType,
+  dateOfEncounterConcept,
+  expressCareProgramStatusConcept,
+  regimenConcept,
+  returnVisitDateConcept,
+  visitTypeConcept,
+} from '../../../constants';
+import EncounterList, { EncounterListColumn } from '../../../components/encounter-list/encounter-list.component';
 
 interface ClinicalVisitWidgetProps {
   patientUuid: string;
 }
 
-const ClinicalVisitWidget: React.FC<ClinicalVisitWidgetProps> = ({ patientUuid }) => {
-  const { t } = useTranslation();
-  const [tableRows, setTableRows] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const [clinicalVisitForm, setClinicalVisitForm] = useState(getForm('hiv', 'clinical_visit'));
-
-  const tableHeaders = [
-    { key: 'visitDate', header: 'Visit Date', isSortable: true },
-    { key: 'visitType', header: 'Visit Type' },
-    { key: 'regimen', header: 'Regimen' },
-    { key: 'differentiatedCareService', header: 'Differentiated Care Service' },
-    { key: 'nextAppointmentDate', header: 'Next Appointment Date' },
-    { key: 'action', header: 'Action' },
-  ];
-
-  const loadRows = useCallback(
-    encounterType => {
-      const query = `encounterType=${encounterType}&patient=${patientUuid}`;
-      let rows = [];
-
-      openmrsFetch(`/ws/rest/v1/encounter?${query}&v=${encounterRepresentation}`).then(({ data }) => {
-        // console.log('Response data: ', data);
-      });
+const columns: EncounterListColumn[] = [
+  {
+    conceptUuid: dateOfEncounterConcept,
+    key: 'visitDate',
+    header: 'Visit Date',
+    getValue: encounter => {
+      return 'visit date';
     },
-    [patientUuid],
-  );
+  },
+  {
+    conceptUuid: visitTypeConcept,
+    key: 'visitType',
+    header: 'Visit Type',
+    getValue: encounter => {
+      return 'visit type';
+    },
+  },
+  {
+    conceptUuid: regimenConcept,
+    key: 'regimen',
+    header: 'Regimen',
+    getValue: encounter => {
+      return 'regimen';
+    },
+  },
+  {
+    conceptUuid: expressCareProgramStatusConcept,
+    key: 'differentiatedCareService',
+    header: 'Differentiated Care Service',
+    getValue: encounter => {
+      return 'care service';
+    },
+  },
+  {
+    conceptUuid: returnVisitDateConcept,
+    key: 'nextAppointmentDate',
+    header: 'Next Appointment Date',
+    getValue: encounter => {
+      return 'next appointment';
+    },
+  },
+  {
+    key: 'actions',
+    header: 'Actions',
+    getValue: encounter => {
+      return encounter.actions;
+    },
+  },
+];
 
-  const forceComponentUpdate = () => setCounter(counter + 1);
-
-  const launchClinicalVisitForm = (form?: any) => {
-    launchOHRIWorkSpace('ohri-forms-view-ext', {
-      title: clinicalVisitForm?.name,
-      screenSize: 'maximize',
-      state: { updateParent: forceComponentUpdate, formJson: form || clinicalVisitForm },
-    });
-  };
-
-  useEffect(() => {
-    loadRows(clinicalVisitEncounterType);
-  }, [loadRows, counter]);
-
+const ClinicalVisitWidget: React.FC<ClinicalVisitWidgetProps> = ({ patientUuid }) => {
   return (
-    <>
-      {/*<EncounterList patientUuid={patientUuid} encounterUuid={clinicalVisitEncounterType} columns={} />*/}
-
-      {isLoading ? (
-        <DataTableSkeleton rowCount={5} />
-      ) : tableRows.length > 0 ? (
-        <>
-          <div>Table goes here</div>
-        </>
-      ) : (
-        <EmptyState
-          displayText={t('clinicalVisitEncounters', 'clinical visit encounters')}
-          headerTitle={t('clinicalVisitTitle', 'Clinical Visits')}
-          launchForm={launchClinicalVisitForm}
-          launchFormComponent={
-            <OHRIFormLauncherWithIntent
-              formJson={clinicalVisitForm}
-              launchForm={launchClinicalVisitForm}
-              onChangeIntent={setClinicalVisitForm}
-            />
-          }
-        />
-      )}
-    </>
+    <EncounterList
+      patientUuid={patientUuid}
+      encounterUuid={clinicalVisitEncounterType}
+      form={{ package: 'hiv', name: 'clinical_visit' }}
+      columns={columns}
+      description="clinical visit encounters"
+      headerTitle="Clinical Visits"
+    />
   );
 };
 
