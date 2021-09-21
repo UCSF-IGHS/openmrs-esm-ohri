b423b1cd6b069e0e351687f164326ced0442b806
smallGod <davies.mugume@gmail.com>
2021-09-22 02:52:21 +0300

    OHRI-257 add covid assessment form to Registry

--- src/packages/forms-registry.ts
+++ src/packages/forms-registry.ts
@@ -1,7 +1,7 @@
 import hts_v_1_0 from './hiv/forms/hts/1.0.json';
 import hts_v_2_0 from './hiv/forms/hts/2.0.json';
 import hiv_service_enrolment_v_1_0 from './hiv/forms/care-and-treatment/service-enrolment/1.0.json';
-import covid_case_report_form_v_1_0 from './covid/forms/case-report-form/1.0.json';
+import covid_assessment_form_v_1_0 from './covid/forms/case-assessment-form/1.0.json';
 import clinical_visit_v_1_0 from './hiv/forms/care-and-treatment/clinical-visit/1.0.json';
 
 export default {
@@ -18,8 +18,8 @@ export default {
     },
   },
   covid: {
-    covid_case: {
-      '1.0': covid_case_report_form_v_1_0,
+    covid_assessment: {
+      '1.0': covid_assessment_form_v_1_0,
     },
   },
 };
