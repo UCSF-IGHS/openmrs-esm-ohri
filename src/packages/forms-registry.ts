7e7708264ba18575e75b9d6bfdf43a393779d7ac
smallGod <davies.mugume@gmail.com>
2021-09-22 03:52:56 +0300

    OHRI-258 Add COVID lab test form

--- src/packages/forms-registry.ts
+++ src/packages/forms-registry.ts
@@ -2,6 +2,7 @@ import hts_v_1_0 from './hiv/forms/hts/1.0.json';
 import hts_v_2_0 from './hiv/forms/hts/2.0.json';
 import hiv_service_enrolment_v_1_0 from './hiv/forms/care-and-treatment/service-enrolment/1.0.json';
 import covid_assessment_form_v_1_0 from './covid/forms/case-assessment-form/1.0.json';
+import covid_lab_test_form_v_1_0 from './covid/forms/lab-test-form/1.0.json';
 import clinical_visit_v_1_0 from './hiv/forms/care-and-treatment/clinical-visit/1.0.json';
 
 export default {
@@ -20,6 +21,9 @@ export default {
   covid: {
     covid_assessment: {
       '1.0': covid_assessment_form_v_1_0,
+    },
+    covid_lab_test_form_v_1_0: {
+      '1.0': covid_lab_test_form_v_1_0,
     },
   },
 };
