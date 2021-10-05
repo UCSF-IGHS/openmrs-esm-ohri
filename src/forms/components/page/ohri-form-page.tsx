f78c575ac7c07094da933117300bfee4d3cab885
pirupius <piruville@gmail.com>
2021-10-05 12:58:50 +0300

    OHRI-276 apply design changes

--- src/forms/components/page/ohri-form-page.tsx
+++ src/forms/components/page/ohri-form-page.tsx
@@ -3,7 +3,6 @@ import styles from './_page.scss';
 import OHRIFormSection from '../section/ohri-form-section.component';
 import { Waypoint } from 'react-waypoint';
 import { Accordion, AccordionItem, Toggle } from 'carbon-components-react';
-import { Collapse } from 'react-collapse';
 
 function OHRIFormPage({ page, onFieldChange, setSelectedPage }) {
   let newLabel = page.label.replace(/\s/g, '');
@@ -20,9 +19,9 @@ function OHRIFormPage({ page, onFieldChange, setSelectedPage }) {
   return (
     <Waypoint onEnter={() => handleEnter(newLabel)} bottomOffset="95%">
       <div id={newLabel} className={styles.pageContent}>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{}} className={styles.pageHeader}>
           <p className={styles.pageTitle}>{page.label}</p>
-          <div style={{ marginLeft: '-300px', marginBottom: '10px' }}>
+          <div className={styles.collapseToggle}>
             <Toggle
               size="sm"
               aria-label="toggle button"
@@ -34,26 +33,24 @@ function OHRIFormPage({ page, onFieldChange, setSelectedPage }) {
             />
           </div>
         </div>
-        <Collapse isOpened={true}>
-          <Accordion>
-            {/* <p className={styles.required}>All fields are required unless marked optional</p> */}
-            {page.sections.map((sec, index) => {
-              return (
-                <AccordionItem title={sec.label} open={isCollapsed}>
-                  <div className={styles.formSection} key={index}>
-                    <OHRIFormSection
-                      fields={sec.questions}
-                      showTitle={page.sections.length > 1}
-                      onFieldChange={onFieldChange}
-                      sectionTitle={sec.label}
-                      key={index}
-                    />
-                  </div>
-                </AccordionItem>
-              );
-            })}
-          </Accordion>
-        </Collapse>
+        <Accordion>
+          {/* <p className={styles.required}>All fields are required unless marked optional</p> */}
+          {page.sections.map((sec, index) => {
+            return (
+              <AccordionItem title={sec.label} open={isCollapsed} className={styles.sectionContent}>
+                <div className={styles.formSection} key={index}>
+                  <OHRIFormSection
+                    fields={sec.questions}
+                    showTitle={page.sections.length > 1}
+                    onFieldChange={onFieldChange}
+                    sectionTitle={sec.label}
+                    key={index}
+                  />
+                </div>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
         <hr className={styles.divider} />
       </div>
     </Waypoint>
