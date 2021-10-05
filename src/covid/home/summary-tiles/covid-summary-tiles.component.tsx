d3a88ef643b29561097eae6c97e501189df24ead
Samuel Male <samuelsmalek@gmail.com>
2021-10-05 22:20:08 +0300

    Update Clients with Assessments list and other improvements

--- src/covid/home/summary-tiles/covid-summary-tiles.component.tsx
+++ src/covid/home/summary-tiles/covid-summary-tiles.component.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { getReportingCohort } from '../../../api/api';
 import OHRIProgrammeSummaryTiles from '../../../components/tile/ohri-programme-summary-tiles.component';
 import { covidVaccinatedClients } from '../../../constants';
 
