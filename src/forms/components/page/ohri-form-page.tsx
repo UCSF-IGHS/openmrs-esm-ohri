b496486d25fffb133678f837f22eac991e82a3d8
Rubangakene Pius <piruville@gmail.com>
2021-10-07 12:12:05 +0300

    OHRI-276 collapsing form sections clean (#427)
    * OHRI-276 collapsing form sections
    
    * collapsable form sections
    
    * adds missing action to collapse all sections
    
    * OHRI-276 apply design changes
    
    * changes field background to white
    
    * removes horizontal divider and adds bottom border to fields
    
    * fixes rebase issues

--- src/forms/components/page/ohri-form-page.tsx
+++ src/forms/components/page/ohri-form-page.tsx
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './_page.scss';
 import OHRIFormSection from '../section/ohri-form-section.component';
 import { Waypoint } from 'react-waypoint';
+import { Accordion, AccordionItem, Toggle } from 'carbon-components-react';
 
 function OHRIFormPage({ page, onFieldChange, setSelectedPage }) {
   let newLabel = page.label.replace(/\s/g, '');
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
   const handleEnter = elementID => {
     setSelectedPage(elementID);
   };
 
+  const toggleCollapsedStatus = e => {
+    setIsCollapsed(e);
+  };
+
   return (
     <Waypoint onEnter={() => handleEnter(newLabel)} bottomOffset="95%">
       <div id={newLabel} className={styles.pageContent}>
-        <p className={styles.pageTitle}>{page.label}</p>
-        <p className={styles.required}>All fields are required unless marked optional</p>
-        {page.sections.map((sec, index) => {
-          return (
-            <div className={styles.formSection} key={index}>
-              <OHRIFormSection
-                fields={sec.questions}
-                showTitle={page.sections.length > 1}
-                onFieldChange={onFieldChange}
-                sectionTitle={sec.label}
-                key={index}
-              />
-            </div>
-          );
-        })}
-        <hr className={styles.divider} />
+        <div style={{}} className={styles.pageHeader}>
+          <p className={styles.pageTitle}>{page.label}</p>
+          <div className={styles.collapseToggle}>
+            <Toggle
+              size="sm"
+              aria-label="toggle button"
+              defaultToggled
+              id={`${newLabel}-toggle`}
+              labelA="Expand sections"
+              labelB="Collapse sections"
+              onToggle={toggleCollapsedStatus}
+            />
+          </div>
+        </div>
+        <Accordion>
+          {/* <p className={styles.required}>All fields are required unless marked optional</p> */}
+          {page.sections.map((sec, index) => {
+            return (
+              <AccordionItem title={sec.label} open={isCollapsed} className={styles.sectionContent}>
+                <div className={styles.formSection} key={index}>
+                  <OHRIFormSection
+                    fields={sec.questions}
+                    showTitle={page.sections.length > 1}
+                    onFieldChange={onFieldChange}
+                    sectionTitle={sec.label}
+                    key={index}
+                  />
+                </div>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
       </div>
     </Waypoint>
   );
