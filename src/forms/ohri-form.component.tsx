4e6802078657402fca491cb709a92864f3b96133
Lars Albino Lemos <lars.lemos@ucsf.edu>
2021-10-07 10:42:58 +0200

    Feature/ohri 209 form engine - Synch feature into dev (#421)
    * Add group obs files
    
    * Introduce repeat control
    
    * Update HTS form json
    
    * Fix submission of null obsgroups
    
    * Construct repeating obs groups from encounter
    
    * Fix intermittent issue related to value binding with the Multiselect control
    
    * Add support for voiding obsgroup and other improvements
    
    * OHRI-274 Form Render UX Improvements
    
    * wip form render test ui
    
    * More improvements to make things work :pray:
    
    * OHRI-274 form render UI changes
    
    * form render test intellisense
    
    * renames class
    
    * design review changes
    
    * adds json editor theme selector and adds media queries
    
    * adds intellisense to final schema output
    
    * Fix submission of empty obsgroups and comprehensive support of 'Unspecified' within obsgroups :surfer:
    
    * filterFormByIntent preprocessor to support nested questions :rocket: (#371)
    
    * Fix lists not loading in Homescreens (#381)
    
    * Skip patterns to support iterables :hammer: (#396)
    
    * Disabled and Hidden fields should not be validated (#401)
    
    Co-authored-by: Samuel Male <samuelsmalek@gmail.com>
    Co-authored-by: pirupius <piruville@gmail.com>

--- src/forms/ohri-form.component.tsx
+++ src/forms/ohri-form.component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './_form.scss';
 import { Form, Formik } from 'formik';
 import * as Yup from 'yup';
@@ -13,7 +13,7 @@ import { OHRIFormSchema, OHRIFormField, SessionMode } from './types';
 import OHRIFormSidebar from './components/sidebar/ohri-form-sidebar.component';
 import OHRIFormPage from './components/page/ohri-form-page';
 import { ConceptFalse, ConceptTrue, HTSEncounterType } from './constants';
-import { isEmpty as isValueEmpty, OHRIFieldValidator } from './ohri-form-validator';
+import { isEmpty, isEmpty as isValueEmpty, OHRIFieldValidator } from './ohri-form-validator';
 import { encounterRepresentation } from '../constants';
 
 interface OHRIFormProps {
@@ -46,17 +46,32 @@ const OHRIForm: React.FC<OHRIFormProps> = ({
   const [form, setForm] = useState<OHRIFormSchema>(null);
   const [scrollAblePages, setScrollAblePages] = useState(undefined);
   const [selectedPage, setSelectedPage] = useState('');
+  const [obsGroupsToVoid, setObsGroupsToVoid] = useState([]);
   const { t } = useTranslation();
 
   useEffect(() => {
     const form = JSON.parse(JSON.stringify(formJson));
     const allFormFields: Array<OHRIFormField> = [];
     const tempInitVals = {};
-    form.pages.forEach(page => page.sections.forEach(section => allFormFields.push(...section.questions)));
+    form.pages.forEach(page =>
+      page.sections.forEach(section => {
+        section.questions.forEach(question => {
+          allFormFields.push(question);
+          if (question.type == 'obsGroup') {
+            question.questions.forEach(groupedField => {
+              // set group id
+              groupedField['groupId'] = question.id;
+              allFormFields.push(groupedField);
+            });
+          }
+        });
+      }),
+    );
     // set Formik initial values
     if (encounter) {
       allFormFields.forEach(field => {
-        const existingVal = getHandler(field.type)?.getInitialValue(encounter, field);
+        const existingVal = getHandler(field.type)?.getInitialValue(encounter, field, allFormFields);
+
         tempInitVals[field.id] = existingVal === null || existingVal === undefined ? '' : existingVal;
         if (field.unspecified) {
           tempInitVals[`${field.id}-unspecified`] = !!!existingVal;
@@ -137,6 +152,19 @@ const OHRIForm: React.FC<OHRIFormProps> = ({
       }
       return isValueEmpty(value);
     }
+    function includes(questionId, value) {
+      if (allFieldsKeys.includes(questionId)) {
+        const determinant = allFields.find(candidate => candidate.id === questionId);
+        if (!determinant.fieldDependants) {
+          determinant.fieldDependants = new Set();
+        }
+        determinant.fieldDependants.add(field.id);
+        const initValues = initialVals || initialValues;
+        const valueArray = determinantValue || initValues[questionId];
+        return valueArray?.includes(value);
+      }
+      return false;
+    }
     parts.forEach((part, index) => {
       if (index % 2 == 0) {
         if (allFieldsKeys.includes(part)) {
@@ -186,12 +214,48 @@ const OHRIForm: React.FC<OHRIFormProps> = ({
     }
   };
 
+  const addObs = useCallback((obsList: Array<any>, obs: any) => {
+    if (Array.isArray(obs)) {
+      obs.forEach(o => {
+        delete o.formFieldNamespace;
+        delete o.formFieldPath;
+        if (isEmpty(o.groupMembers)) {
+          delete o.groupMembers;
+        } else {
+          o.groupMembers.forEach(obsChild => {
+            delete obsChild.formFieldNamespace;
+            delete obsChild.formFieldPath;
+            if (isEmpty(obsChild.groupMembers)) {
+              delete obsChild.groupMembers;
+            }
+          });
+        }
+        obsList.push(o);
+      });
+    } else {
+      delete obs.formFieldNamespace;
+      delete obs.formFieldPath;
+      if (isEmpty(obs.groupMembers)) {
+        delete obs.groupMembers;
+      } else {
+        obs.groupMembers.forEach(obsChild => {
+          delete obsChild.formFieldNamespace;
+          delete obsChild.formFieldPath;
+          if (isEmpty(obsChild.groupMembers)) {
+            delete obsChild.groupMembers;
+          }
+        });
+      }
+      obsList.push(obs);
+    }
+  }, []);
+
   const handleFormSubmit = (values: Record<string, any>) => {
     const obsForSubmission = [];
     let formHasErrors = false;
     // handle field validation
     fields
-      .filter(field => !field.disabled || !field.isHidden)
+      .filter(field => !field.disabled && !field.isHidden)
       .filter(field => field['submission']?.unspecified != true)
       .forEach(field => {
         const errors = OHRIFieldValidator.validate(field, values[field.id]);
@@ -209,15 +273,40 @@ const OHRIForm: React.FC<OHRIFormProps> = ({
     }
     // collect observations
     fields
-      .filter(field => !field.isHidden && field.type == 'obs' && field.value)
+      .filter(field => field.value || field.type == 'obsGroup') // filter out fields with empty values except groups
+      .filter(field => !field.isHidden && (field.type == 'obs' || field.type == 'obsGroup'))
+      .filter(field => !field['groupId']) // filter out grouped obs
       .forEach(field => {
-        if (Array.isArray(field.value)) {
-          obsForSubmission.push(...field.value);
+        if (field.type == 'obsGroup') {
+          const obsGroup = {
+            person: patient.id,
+            obsDatetime: encDate,
+            concept: field.questionOptions.concept,
+            location: location,
+            order: null,
+            groupMembers: [],
+            uuid: field?.value?.uuid,
+            voided: false,
+          };
+          let hasValue = false;
+          field.questions.forEach(groupedField => {
+            if (groupedField.value) {
+              hasValue = true;
+              if (Array.isArray(groupedField.value)) {
+                obsGroup.groupMembers.push(...groupedField.value);
+              } else {
+                obsGroup.groupMembers.push(groupedField.value);
+              }
+            }
+          });
+          hasValue && addObs(obsForSubmission, obsGroup);
         } else {
-          obsForSubmission.push(field.value);
+          addObs(obsForSubmission, field.value);
         }
       });
 
+    // Add voided obs groups
+    obsGroupsToVoid.forEach(obs => addObs(obsForSubmission, obs));
     let encounterForSubmission = {};
     if (encounter) {
       Object.assign(encounterForSubmission, encounter);
@@ -306,7 +395,6 @@ const OHRIForm: React.FC<OHRIFormProps> = ({
       });
     }
   };
-
   return (
     <Formik
       enableReinitialize
@@ -340,6 +428,8 @@ const OHRIForm: React.FC<OHRIFormProps> = ({
                       values: props.values,
                       setFieldValue: props.setFieldValue,
                       setEncounterLocation: setEncounterLocation,
+                      setObsGroupsToVoid: setObsGroupsToVoid,
+                      obsGroupsToVoid: obsGroupsToVoid,
                       fields: fields,
                       encounterContext: {
                         patient: patient,
--- src/workspace/ohri-workspace.component.tsx
+++ src/workspace/ohri-workspace.component.tsx
@@ -1,13 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { ExtensionSlot } from '@openmrs/esm-framework';
+import { ExtensionSlot, useAssignedExtensionIds } from '@openmrs/esm-framework';
 import { workspaceContext, WorkspaceContextProps } from './ohri-workspace-utils';
 import styles from './patient-list-workspace.scss';
+
+export let workspaceInUse = false;
 
 const OHRIWorkspace: React.FC<{ closeWorkspace: () => {}; patientUuid: string; viewMode: string }> = ({
   closeWorkspace,
   patientUuid,
   viewMode,
 }) => {
+  const currentExtensions = useAssignedExtensionIds('patient-chart-workspace-slot');
+
+  useEffect(() => {
+    if (currentExtensions.length == 0) {
+      workspaceInUse = false;
+    } else {
+      workspaceInUse = true;
+    }
+    return () => {
+      workspaceInUse = false;
+    };
+  }, [currentExtensions]);
+
   const [context, setContext] = useState<WorkspaceContextProps>(null);
   useEffect(() => {
     const sub = workspaceContext.subscribe(context => setContext(context));
