87c404709b7038b4f6d031eaf27d403c7c5df4cb
Lars Albino Lemos <lars.lemos@ucsf.edu>
2021-10-17 11:13:29 +0200

    Apply generic encounter list widget and fix Launcher
    * Fix Encounter List Page size, refactor HTS encounter to generic widget
    
    * Add Generic encounter to CD4 widget, fix luancher

--- src/pages/lab-results/encounter-list/lab-results-encounter-list.component.tsx
+++ src/pages/lab-results/encounter-list/lab-results-encounter-list.component.tsx
@@ -1,12 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import EmptyStateComingSoon from '../../../components/empty-state/empty-state-comingsoon.component';
+import EmptyState from '../../../components/empty-state/empty-state.component';
 
-interface OverviewListProps {
+import EncounterList, {
+  EncounterListColumn,
+  getEncounterValues,
+  getObsFromEncounter,
+} from '../../../components/encounter-list/encounter-list.component';
+import { hivLabResultsEncounterType_UUID, hivCD4Count_UUID, hivCD4Result_UUID } from '../../../constants';
+
+interface LabResultsOverviewListProps {
   patientUuid: string;
 }
 
-const LabResultsOverviewList: React.FC<OverviewListProps> = ({ patientUuid }) => {
+const columns: EncounterListColumn[] = [
+  {
+    key: 'encounterDate',
+    header: 'Date of Test ordered',
+    getValue: encounter => {
+      return getEncounterValues(encounter, 'encounterDateTime', true);
+    },
+  },
+  {
+    key: 'location',
+    header: 'Location',
+    getValue: encounter => {
+      return encounter.location.name || 'None';
+    },
+  },
+  {
+    key: 'hivLabResult',
+    header: 'CD4 Date Result',
+    getValue: encounter => {
+      return getObsFromEncounter(encounter, hivCD4Result_UUID);
+    },
+  },
+  {
+    key: 'hivCD4Count',
+    header: 'CD4 Count',
+    getValue: encounter => {
+      return getObsFromEncounter(encounter, hivCD4Count_UUID);
+    },
+  },
+  {
+    key: 'actions',
+    header: 'Actions',
+    getValue: () => {},
+  },
+];
+
+const LabResultsOverviewList: React.FC<LabResultsOverviewListProps> = ({ patientUuid }) => {
   const { t } = useTranslation();
 
   const headerTitle = t('labResults', 'Viral Load');
@@ -14,7 +57,7 @@ const LabResultsOverviewList: React.FC<OverviewListProps> = ({ patientUuid }) =>
 
   return (
     <>
-      <EmptyStateComingSoon headerTitle={headerTitle} displayText={displayText} />
+      <EmptyState displayText={displayText} headerTitle={headerTitle} />
     </>
   );
 };
