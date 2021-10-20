6cf257c3a9c75fdf5d91ba02f7b273dcd85433e5
pirupius <piruville@gmail.com>
2021-10-20 09:21:16 +0300

    OHRI-309 replace intent dropdown with overflow menu

--- src/components/ohri-form-launcher/ohri-form-launcher.component.tsx
+++ src/components/ohri-form-launcher/ohri-form-launcher.component.tsx
@@ -4,6 +4,7 @@ import { Add16 } from '@carbon/icons-react';
 import styles from './laucher-with-intent.scss';
 import { filterFormByIntent } from '../../utils/forms-loader';
 import { useTranslation } from 'react-i18next';
+import { launchOHRIWorkSpace } from '../../workspace/ohri-workspace-utils';
 
 export const OHRIFormLauncherWithIntent: React.FC<{
   formJson: any;
@@ -12,33 +13,51 @@ export const OHRIFormLauncherWithIntent: React.FC<{
   dropDownText?: string;
   hideFormLauncher?: boolean;
 }> = ({ formJson, launchForm, onChangeIntent, dropDownText, hideFormLauncher }) => {
-  // Keeping this here for now, we need to figureout how to enforce users to select an intent when launching a form in edit mode
+  // Keeping this here for now, we need to figure out how to enforce users to select an intent when launching a form in edit mode
   // This should be thrown away after the above is resolved
   // const [processedForm, setProcessedForm] = useState(null);
+  const { t } = useTranslation();
 
   //   useEffect(() => {
   //     const processed = filterFormByIntent('*', formJson);
   //     setProcessedForm(processed);
   //     onChangeIntent(processed);
   //   }, []);
-  const { t } = useTranslation();
-
-  // console.log(formJson.availableIntents);
 
   return (
     <div style={{ paddingTop: '.3rem', paddingRight: '.5rem', width: '13rem' }}>
       {!hideFormLauncher && (
-        <OverflowMenu flipped className={styles.flippedOverflowMenu}>
-          {formJson.availableIntents.forEach(intent => {
-            <OverflowMenuItem
-              itemText={t(intent.intent, intent.display)}
-              onClick={e => {
-                e.preventDefault();
-                // viewEncounter(encounter.uuid);
-              }}
-            />;
+        <OverflowMenu
+          flipped={true}
+          className={styles.flippedOverflowMenu}
+          renderIcon={Add16}
+          iconDescription="Add"
+          ariaLabel="Add">
+          {formJson.availableIntents.map((intent, index) => {
+            return (
+              <OverflowMenuItem
+                itemText={intent.display}
+                onClick={e => {
+                  e.preventDefault();
+                  const processedForm = filterFormByIntent(intent.intent, formJson);
+                  launchForm(processedForm);
+                }}
+              />
+            );
           })}
         </OverflowMenu>
+
+        // <Button
+        //   kind="ghost"
+        //   renderIcon={Add16}
+        //   onClick={e => {
+        //     e.preventDefault();
+        //     // launchForm();
+        //     intentActionOverflowMenu;
+        //   }}>
+        //   {t('add', 'Add')}
+        // </Button>
+
         // <Dropdown
         //   id="choose-intent"
         //   label={dropDownText || 'New'}
