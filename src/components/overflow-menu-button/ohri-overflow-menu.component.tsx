d088ad6d6aa51f55496c1d95fa12e04e14700ff2
Rubangakene Pius <piruville@gmail.com>
2021-10-22 14:06:44 +0300

    OHRI-304 patient chart UI fixes (#507)
    * OHRI-310 fix broken table headers when workspace is open
    
    * replace intent dropdown with overflow
    
    * OHRI-309 replace intent dropdown with overflow menu
    
    * overflow menu button

---
+++ src/components/overflow-menu-button/ohri-overflow-menu.component.tsx
@@ -0,0 +1,108 @@
+import React, { useState, useCallback, useEffect, useRef } from 'react';
+import styles from './ohri-overflow-menu.scss';
+import { useTranslation } from 'react-i18next';
+import { filterFormByIntent } from '../../utils/forms-loader';
+
+interface OverflowMenuProps {
+  menuTitle: React.ReactNode;
+  overflowItems: Array<any>;
+  overflowIcon?: any;
+  launchForm?: (formJson?: any) => void;
+  formJson?: any;
+}
+
+export const OHRIOverflowMenu: React.FC<OverflowMenuProps> = ({
+  menuTitle,
+  overflowIcon,
+  overflowItems,
+  launchForm,
+  formJson,
+}) => {
+  const { t } = useTranslation();
+  const [showMenu, setShowMenu] = useState(false);
+  const wrapperRef = useRef(null);
+  const toggleShowMenu = useCallback(() => setShowMenu(state => !state), []);
+
+  useEffect(() => {
+    /**
+     * Toggle showMenu if clicked on outside of element
+     */
+    function handleClickOutside(event: MouseEvent) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [wrapperRef]);
+
+  return (
+    <div className={`bx--overflow-menu ${styles.overflowMenuContainer}`} ref={wrapperRef}>
+      <button
+        className={`bx--overflow-menu__trigger ${showMenu && 'bx--overflow-menu--open'}`}
+        aria-haspopup="true"
+        aria-expanded={showMenu}
+        id="custom-actions-overflow-menu-trigger"
+        aria-controls="custom-actions-overflow-menu"
+        onClick={toggleShowMenu}
+        style={{
+          width: 'auto',
+          height: 'auto',
+          padding: '1rem',
+          color: '#0f62fe',
+          outline: '2rem solid transparent',
+          boxShadow: showMenu ? '0 2px 6px 0 rgb(0 0 0 / 30%)' : 'none',
+        }}>
+        {menuTitle}
+      </button>
+      <div
+        className="bx--overflow-menu-options bx--overflow-menu--flip"
+        tabIndex={0}
+        data-floating-menu-direction="bottom"
+        role="menu"
+        aria-labelledby="custom-actions-overflow-menu-trigger"
+        id="custom-actions-overflow-menu"
+        style={{
+          display: showMenu ? 'block' : 'none',
+          top: '3.125rem',
+          minWidth: 'initial',
+          left: 'auto',
+          right: '0',
+          backgroundColor: '#f4f4f4',
+          marginRight: '0.2rem',
+          boxShadow: '0 6px 6px rgb(0 0 0 / 30%)',
+        }}>
+        <ul className="bx--overflow-menu-options__content">
+          {overflowItems.map((menuItem, index) => {
+            return (
+              <li className="bx--overflow-menu-options__option" id={'item-' + index}>
+                <button
+                  id={'menuItem-' + index}
+                  className="bx--overflow-menu-options__btn"
+                  role="menuitem"
+                  title={t('endVisit', 'End Visit')}
+                  onClick={e => {
+                    e.preventDefault();
+                    const processedForm = filterFormByIntent(menuItem.intent, formJson);
+                    launchForm(processedForm);
+                    setShowMenu(false);
+                  }}
+                  style={{
+                    maxWidth: '100vw',
+                  }}>
+                  <span className="bx--overflow-menu-options__option-content">{menuItem.display}</span>
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        <span />
+      </div>
+    </div>
+  );
+};
