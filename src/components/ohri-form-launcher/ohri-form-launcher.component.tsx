697114f3062a49b97b77fe9a81456bf23f6bdba8
pirupius <piruville@gmail.com>
2021-10-22 13:04:30 +0300

    overflow menu button

--- src/components/ohri-form-launcher/ohri-form-launcher.component.tsx
+++ src/components/ohri-form-launcher/ohri-form-launcher.component.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { Button, Dropdown, OverflowMenu, OverflowMenuItem } from 'carbon-components-react';
 import { Add16 } from '@carbon/icons-react';
-import styles from './laucher-with-intent.scss';
+import styles from './launcher-with-intent.scss';
 import { filterFormByIntent } from '../../utils/forms-loader';
 import { useTranslation } from 'react-i18next';
 import { launchOHRIWorkSpace } from '../../workspace/ohri-workspace-utils';
+import { OHRIOverflowMenu } from '../overflow-menu-button/ohri-overflow-menu.component';
 
 export const OHRIFormLauncherWithIntent: React.FC<{
   formJson: any;
@@ -25,53 +25,19 @@ export const OHRIFormLauncherWithIntent: React.FC<{
   //   }, []);
 
   return (
-    <div style={{ paddingTop: '.3rem', paddingRight: '.5rem', width: '13rem' }}>
+    <div style={{ paddingTop: '.3rem' }}>
       {!hideFormLauncher && (
-        <OverflowMenu
-          flipped={true}
-          className={styles.flippedOverflowMenu}
-          renderIcon={Add16}
-          iconDescription="Add"
-          ariaLabel="Add">
-          {formJson.availableIntents.map((intent, index) => {
-            return (
-              <OverflowMenuItem
-                itemText={intent.display}
-                onClick={e => {
-                  e.preventDefault();
-                  const processedForm = filterFormByIntent(intent.intent, formJson);
-                  launchForm(processedForm);
-                }}
-              />
-            );
-          })}
-        </OverflowMenu>
-
-        // <Button
-        //   kind="ghost"
-        //   renderIcon={Add16}
-        //   onClick={e => {
-        //     e.preventDefault();
-        //     // launchForm();
-        //     intentActionOverflowMenu;
-        //   }}>
-        //   {t('add', 'Add')}
-        // </Button>
-
-        // <Dropdown
-        //   id="choose-intent"
-        //   label={dropDownText || 'New'}
-        //   items={formJson.availableIntents}
-        //   titleText=""
-        //   selectedItem={null}
-        //   onChange={({ selectedItem }) => {
-        //     const processedForm = filterFormByIntent(selectedItem.intent, formJson);
-        //     // setProcessedForm(processedForm);
-        //     // onChangeIntent(processedForm);
-        //     launchForm(processedForm);
-        //   }}
-        //   itemToString={item => item.display}
-        // />
+        <OHRIOverflowMenu
+          menuTitle={
+            <>
+              <span className={styles.actionsButtonText}>{t('add', 'Add')}</span>{' '}
+              <Add16 style={{ marginLeft: '0.5rem' }} />
+            </>
+          }
+          overflowItems={formJson.availableIntents}
+          launchForm={launchForm}
+          formJson={formJson}
+        />
       )}
     </div>
   );
