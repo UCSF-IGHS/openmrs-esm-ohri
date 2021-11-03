a74b027439d223eb9d3e5cd22a6970eb946c51b1
amuj <alaboso@uonbi.ac.ke>
2021-11-03 10:23:39 +0300

    OHRI-363: COVID Lab Result Form

--- src/packages/forms-registry.ts
+++ src/packages/forms-registry.ts
@@ -8,6 +8,8 @@ import covid_vaccination_form_v_1_0 from './covid/forms/vaccination-form/1.0.jso
 import clinical_visit_v_1_0 from './hiv/forms/care-and-treatment/clinical-visit/1.0.json';
 import lab_results_v_1_0 from './hiv/forms/care-and-treatment/lab-results/1.0.json';
 import covid_sample_collection_form_v_1_0 from './covid/forms/sample-collection-form/1.0.json';
+import covid_lab_order_form_v_1_0 from './covid/forms/lab-order-form/1.0.json';
+import covid_lab_result_form_v_1_0 from './covid/forms/lab-result-form/1.0.json';
 
 export default {
   hiv: {
@@ -43,6 +45,12 @@ export default {
     },
     covid_sample_collection: {
       '1.0': covid_sample_collection_form_v_1_0,
+    },
+    covid_lab_order: {
+      '1.0': covid_lab_order_form_v_1_0,
+    },
+    covid_lab_result: {
+      '1.0': covid_lab_result_form_v_1_0,
     },
   },
 };
