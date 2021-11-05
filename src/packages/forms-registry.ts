b6c34be9eb92325055fdc06223db295707b9f177
Arthur D. Mugume <davies.mugume@gmail.com>
2021-11-05 13:28:28 +0300

    Add Assessment, lab test, vaccination & subform json schema
    * OHRI-281 Assessment summary view
    
    * OHRI-361 Add Lab order cancellation form
    
    * Add subform behaviour & other forms
    
    * OHRI-372 Add 'booster' answer
    
    * OHRI-361 Add Lab order form default intent name
    
    * OHRI-360 Correct the embedded subforms json
    
    * OHRI-344 Remove duplicate option 'other' from sample collection form
    
    * OHRI-344 Update 'other' option in Vaccination form
    
    * OHRI-378 Added new sections to Sample collection form
    
    * OHRI-344 Rename tag to 'readonly'
    
    Co-authored-by: amuj <alaboso@uonbi.ac.ke>
    Co-authored-by: Amos Laboso <amos.laboso@gmail.com>

--- src/packages/forms-registry.ts
+++ src/packages/forms-registry.ts
@@ -8,6 +8,8 @@ import covid_vaccination_form_v_1_0 from './covid/forms/vaccination-form/1.0.jso
 import clinical_visit_v_1_0 from './hiv/forms/care-and-treatment/clinical-visit/1.0.json';
 import lab_results_v_1_0 from './hiv/forms/care-and-treatment/lab-results/1.0.json';
 import covid_sample_collection_form_v_1_0 from './covid/forms/sample-collection-form/1.0.json';
+import covid_lab_order_cancellation_form_v_1_0 from './covid/forms/lab-order-cancellation/1.0.json';
+import covid_assessment_summary_v_1_0 from './covid/forms/assessment-summary/1.0.json';
 import covid_lab_order_form_v_1_0 from './covid/forms/lab-order-form/1.0.json';
 import covid_lab_result_form_v_1_0 from './covid/forms/lab-result-form/1.0.json';
 import covid_outcome_form_v_1_0 from './covid/forms/outcome-form/1.0.json';
@@ -48,6 +50,11 @@ export default {
     covid_sample_collection: {
       '1.0': covid_sample_collection_form_v_1_0,
     },
+    covid_lab_order_cancellation: {
+      '1.0': covid_lab_order_cancellation_form_v_1_0,
+    },
+    covid_assessment_summary: {
+      '1.0': covid_assessment_summary_v_1_0,
     covid_lab_order: {
       '1.0': covid_lab_order_form_v_1_0,
     },
