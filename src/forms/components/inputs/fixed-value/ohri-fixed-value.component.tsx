ba2687ac31a6f0fc490af7ea8122d1074f1b28ba
Samuel Male <samuelsmalek@gmail.com>
2021-11-15 19:22:55 +0300

    Add support for obs submission to the 'fixed-value' control (#597)

--- src/forms/components/inputs/fixed-value/ohri-fixed-value.component.tsx
+++ src/forms/components/inputs/fixed-value/ohri-fixed-value.component.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { OHRIFormContext } from '../../../ohri-form-context';
 import { OHRIFormFieldProps } from '../../../types';
-import { OHRILabel } from '../../label/ohri-label.component';
-import { OHRIValueDisplay, OHRIValueEmpty } from '../../value/ohri-value.component';
-import styles from '../_input.scss';
 
-const OHRIFixedValue: React.FC<OHRIFormFieldProps> = ({ question }) =>
-  !question.isHidden && (
-    <div className={styles.formFields}>
-      <OHRILabel value={question.label} />
-      {question.value ? <OHRIValueDisplay value={question.value} /> : <OHRIValueEmpty />}
-    </div>
-  );
+const OHRIFixedValue: React.FC<OHRIFormFieldProps> = ({ question, handler }) => {
+  const { encounterContext } = React.useContext(OHRIFormContext);
+  useEffect(() => {
+    if (question.value && typeof question.value == 'string') {
+      const value = question.value;
+      delete question.value;
+      question.value = handler.handleFieldSubmission(question, value, encounterContext);
+    }
+  }, []);
+  // return (
+  //   !question.isHidden && (
+  //     <div className={styles.formFields}>
+  //       <OHRILabel value={question.label} />
+  //       {question.value ? <OHRIValueDisplay value={question.value?.value} /> : <OHRIValueEmpty />}
+  //     </div>
+  //   )
+  // );
+  return <></>;
+};
 
 export default OHRIFixedValue;
