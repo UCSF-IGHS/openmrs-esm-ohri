58cd7e7f2102950a62b082926bdb2b69c3c21042
Lars Albino Lemos <lars.lemos@ucsf.edu>
2022-01-14 11:44:37 +0200

    Re-desing the structure to adhere to lib

--- packages/esm-form-render-app/src/root.tsx
+++ packages/esm-form-render-app/src/root.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import FormRenderComponent from './render/forms-render.component';
 
-export default function Root () {
+export default function Root() {
   return (
     <BrowserRouter basename={window['getOpenmrsSpaBase']()}>
-      <Route path='/forms/:packageName/:formName/:patientUUID' component={FormRenderComponent} />
+      <Route path="/forms/:packageName/:formName/:patientUUID" component={FormRenderComponent} />
     </BrowserRouter>
   );
 }
