2f1d5371ae20aa488e7d14084106277bf3284fba
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2022-02-15 13:16:03 +0300

    OHRI-573 Assessment should have the updated specifications

--- src/constants.ts
+++ src/constants.ts
@@ -92,6 +92,7 @@ export const covidComorbidityPresentConcept_UUID = '166020AAAAAAAAAAAAAAAAAAAAAA
 export const covidUnderComorbidityConcept_UUID = '0651869c-6e90-48d6-b25c-406270c76bee';
 export const covidPresentSymptonsName_UUID = '12568215-ae1c-42ec-b7e8-8818d2761f46';
 export const covidPatientStatusConcept_UUID = 'de3bc9b7-05b5-41b6-a38d-8d2eec646c4f';
+export const covidEncounterDateTime_UUID = '160753AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 
 //Covid Lab Order
 export const covidLabOrderDate_UUID = '162078AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
