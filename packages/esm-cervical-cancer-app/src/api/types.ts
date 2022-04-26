3a19ae23f501812938b8d9f60be9aafb97a304f3
kajambiya <jssebaggala@outlook.com>
2022-04-26 10:11:06 +0300

    Cervical cancer webpack fixes

--- packages/esm-cervical-cancer-app/src/api/types.ts
+++ packages/esm-cervical-cancer-app/src/api/types.ts
@@ -33,92 +33,6 @@ export interface SessionData {
   links: Links;
 }
 
-export interface AllergyData {
-  allergen: {
-    allergenType: string;
-    codedAllergen: {
-      answers: [];
-      attrributes: [];
-      conceptClass: DisplayMetadata;
-      display: string;
-      links: Links;
-      mappings: DisplayMetadata[];
-      name: {
-        conceptNameType: string;
-        display: string;
-        locale: string;
-        name: string;
-        uuid: string;
-      };
-      names: DisplayMetadata[];
-      setMembers: [];
-      uuid: string;
-    };
-  };
-  auditInfo: {
-    changedBy: DisplayMetadata;
-    creator: DisplayMetadata;
-    dateCreated: string;
-    dateChanged: string;
-  };
-  comment: string;
-  display: string;
-  links: Links;
-  reactions: [
-    {
-      reaction: AllergicReaction;
-    },
-  ];
-  severity: {
-    name: {
-      conceptNameType: string;
-      display: string;
-      locale: string;
-      name: string;
-      uuid: string;
-    };
-    names: DisplayMetadata[];
-    uuid: string;
-  };
-}
-
-export type Allergen = {
-  answers: [];
-  attributes: [];
-  conceptClass: DisplayMetadata;
-  dataType: DisplayMetadata;
-  descriptions: [];
-  display: string;
-  links: Links;
-  mappings: Array<DisplayMetadata>;
-  name: {
-    display: string;
-    links: Links;
-    uuid: string;
-    conceptTypeName?: string;
-    locale: string;
-    localePreferred: boolean;
-    name: string;
-    resourceVersion: string;
-  };
-  names: DisplayMetadata[];
-  setMembers: [];
-  uuid: string;
-};
-
-export type AllergicReaction = {
-  answers: [];
-  attributes: [];
-  conceptClass: DisplayMetadata;
-  datatype: DisplayMetadata;
-  descriptions: DisplayMetadata[];
-  name: {
-    display: string;
-  };
-  display: string;
-  uuid: string;
-};
-
 type Links = Array<{
   rel: string;
   uri: string;
@@ -148,17 +62,6 @@ export interface Location {
   address4?: string;
   address5?: string;
   address6?: string;
-}
-
-export interface HSTEncounter {
-  encounterDatetime: Date;
-  encounterType: string;
-  patient: string;
-  location: string;
-  encounterProviders?: Array<{ encounterRole: string; provider: string }>;
-  obs: Array<any>;
-  form?: string;
-  visit?: string;
 }
 
 export interface Concept {
