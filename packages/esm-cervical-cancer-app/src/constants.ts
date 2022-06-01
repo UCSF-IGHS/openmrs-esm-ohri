b5f85fde311e67e27dfde8744d46be1f6cbc961e
kajambiya <jssebaggala@outlook.com>
2022-06-01 16:52:40 +0300

    added Cacx registration form and list

---
+++ packages/esm-cervical-cancer-app/src/constants.ts
@@ -0,0 +1,4 @@
+export const cacxRegistrationEncounterType_UUID = '3eb24129-9f7d-4372-a14e-c2cf577e5db1';
+export const cervicalCancerScreeningDateConcept = '2f256b46-395b-40f8-b93e-3ea68e5531bc';
+export const previouslyScreenedConcept = '7cf4c96a-3e68-4ca5-9b49-9148adc6263d';
+export const eligibleForScreeningConcept = 'b77ef6d3-4895-4e88-8b17-fa4aff6eba59';
---
+++ packages/esm-cervical-cancer-app/src/forms/forms-registry.ts
@@ -0,0 +1,9 @@
+import cacx_registration_v_1_0 from './cacx-registration/1.0.json';
+
+export default {
+  cacx: {
+    cacx_registration_form: {
+      '1.0': cacx_registration_v_1_0,
+    },
+  },
+};
