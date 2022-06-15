367f6094e796ee5319c2cf8c480e081c52a9fc03
pirupius <piruville@gmail.com>
2022-06-15 10:58:48 +0300

    adds missing jest.config file

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true,
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 10,
+      lines: 10,
+    },
+  },
+  coverageReporters: ['json-summary', 'lcov'],
+  collectCoverageFrom: ['./src/forms/**', '!./src/components/**/*.snap'],
+  transform: {
+    '^.+\\.tsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!@openmrs)'],
+  moduleNameMapper: {
+    '\\.(s?css)$': 'identity-obj-proxy',
+    '^@carbon/icons-react/es/(.*)$': '@carbon/icons-react/lib/$1',
+    '^carbon-components-react/es/(.*)$': 'carbon-components-react/lib/$1',
+    '@openmrs/esm-framework': '@openmrs/esm-framework/mock',
+    'react-i18next': '<rootDir>/__mocks__/react-i18next.js',
+    'lodash-es': 'lodash',
+    'react-markdown': '<rootDir>/__mocks__/react-markdown.tsx',
+  },
+};
+
+module.exports = config;
