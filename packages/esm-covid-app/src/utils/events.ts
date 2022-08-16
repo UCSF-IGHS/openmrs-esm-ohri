507ca15e2f1d35dc1e546bbe1ffa960488b46bfe
pirupius <piruville@gmail.com>
2022-08-16 18:36:29 +0300

    streamlines app shell dependencies and cleans dead code

--- packages/esm-covid-app/src/utils/events.ts
+++
@@ -1,6 +0,0 @@
-const Events = {
-  subscribe: (event: string, callback) => document.addEventListener(event, callback),
-  dispatch: (event: string) => document.dispatchEvent(new CustomEvent(event)),
-};
-
-export default Events;
