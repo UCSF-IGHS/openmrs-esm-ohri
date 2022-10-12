9cfb504899a4af5118534a17b9950604d7ef1e87
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2022-10-12 10:17:59 +0300

    OHRI-1068 Express Visit Form added in OHRI

--- packages/esm-hiv-app/src/views/visits/tabs/express-visit-tab.component.tsx
+++ packages/esm-hiv-app/src/views/visits/tabs/express-visit-tab.component.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { EmptyState } from '@ohri/openmrs-esm-ohri-commons-lib';
+import { EncounterList, EncounterListColumn, getObsFromEncounter } from '@ohri/openmrs-esm-ohri-commons-lib';
+import {
+  ExpressEncounterDate,
+  ExpressRefferalReason,
+  ExpressTBOutcome,
+  ExpressVisitEncounterType,
+  ExpressVisitType,
+} from '../../../constants';
 
 interface ExpressVisitListProps {
   patientUuid: string;
@@ -9,13 +16,81 @@ interface ExpressVisitListProps {
 const ExpressVisitList: React.FC<ExpressVisitListProps> = ({ patientUuid }) => {
   const { t } = useTranslation();
 
+  const columns: EncounterListColumn[] = useMemo(
+    () => [
+      {
+        key: 'visitDate',
+        header: t('ExpressVisitDate', 'Visit Date'),
+        getValue: encounter => {
+          return getObsFromEncounter(encounter, ExpressEncounterDate, true);
+        },
+        link: {
+          getUrl: encounter => encounter.url,
+          handleNavigate: encounter => {
+            encounter.launchFormActions?.viewEncounter();
+          },
+        },
+      },
+      {
+        key: 'visitType',
+        header: t('ExpressVisitType', 'Visit Type'),
+        getValue: encounter => {
+          return getObsFromEncounter(encounter, ExpressVisitType);
+        },
+      },
+      {
+        key: 'screeningOutcome',
+        header: t('ExpressTB', 'TB Screening Outcome'),
+        getValue: encounter => {
+          return getObsFromEncounter(encounter, ExpressTBOutcome);
+        },
+      },
+      {
+        key: 'appointmentReason',
+        header: t('ExpressRefferal', 'Refferal Reason'),
+        getValue: encounter => {
+          return getObsFromEncounter(encounter, ExpressRefferalReason);
+        },
+      },
+      {
+        key: 'actions',
+        header: t('actions', 'Actions'),
+        getValue: encounter => {
+          const baseActions = [
+            {
+              form: { name: 'exress_visit', package: 'hiv' },
+              encounterUuid: encounter.uuid,
+              intent: '*',
+              label: t('ExpressViewDetails', 'View Details'),
+              mode: 'view',
+            },
+            {
+              form: { name: 'exress_visit', package: 'hiv' },
+              encounterUuid: encounter.uuid,
+              intent: '*',
+              label: t('ExpressEditForm', 'Edit Form'),
+              mode: 'edit',
+            },
+          ];
+          return baseActions;
+        },
+      },
+    ],
+    [],
+  );
+
   const headerTitle = t('expressVisit', 'Express Visit');
-  const displayText = t('expressVisit', 'Express Visit');
+  const displayText = t('expressVisit', 'Express visit encounters');
 
   return (
-    <>
-      <EmptyState displayText={displayText} headerTitle={headerTitle} />
-    </>
+    <EncounterList
+      patientUuid={patientUuid}
+      encounterUuid={ExpressVisitEncounterType}
+      form={{ package: 'hiv', name: 'exress_visit' }}
+      columns={columns}
+      description={displayText}
+      headerTitle={headerTitle}
+    />
   );
 };
 
