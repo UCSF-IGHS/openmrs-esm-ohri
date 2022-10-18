c98d03fbc1af1d6a31e9e2a3bc16a5c1fcd630cb
pirupius <piruville@gmail.com>
2022-10-18 10:13:21 +0300

    OHRI-1081 esm ohri pmtct

---
+++ packages/esm-ohri-pmtct/src/constants.ts
@@ -0,0 +1,1 @@
+export const mnchEncounterType_UUID = '3eb24129-9f7d-4372-a14e-c2cf577e5db1';
---
+++ packages/esm-ohri-pmtct/src/dashboard.meta.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import styles from './dashboard.scss';
+
+export const mnchSummary_dashboardMeta = {
+  slot: 'mnch-summary-slot',
+  columns: 1,
+  title: 'MNCH Summary',
+};
+
+export const mchFolderMeta = {
+  title: 'Maternal & Child Health',
+  slotName: 'mch-slot',
+  isExpanded: false,
+};
