bc510444d499447b6f3672bf32e0c1ab10cbbabf
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2022-11-10 09:39:53 +0300

    OHRI-1092 Mother - Postnatal Form added in PTracker

--- packages/esm-ohri-pmtct-app/src/forms/forms-registry.ts
+++ packages/esm-ohri-pmtct-app/src/forms/forms-registry.ts
@@ -1,9 +1,13 @@
 import labour_and_delivery_v_1_0 from './labour-and-delivery/1.0.json';
+import mother_postnatal_v_1_0 from './mother-postnatal/1.0.json';
 
 export default {
   maternal_health: {
     labour_and_delivery: {
       '1.0': labour_and_delivery_v_1_0,
+    },
+    mother_postanatal_form: {
+      '1.0': mother_postnatal_v_1_0,
     },
   },
 };
