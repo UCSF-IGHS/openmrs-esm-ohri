dcebd3bb2ca29767a94efa5ca71bdd9b0c12a650
kajambiya <jssebaggala@outlook.com>
2022-11-11 18:03:37 +0300

    fix broken registry

--- packages/esm-ohri-pmtct-app/src/forms/forms-registry.ts
+++ packages/esm-ohri-pmtct-app/src/forms/forms-registry.ts
@@ -8,6 +8,9 @@ export default {
     labour_and_delivery: {
       '1.0': labour_and_delivery_v_1_0,
     },
+    antenatal: {
+      '1.0': antenatal_v_1_0,
+    },
     mother_postanatal_form: {
       '1.0': mother_postnatal_v_1_0,
     },
@@ -15,9 +18,6 @@ export default {
   child_health: {
     infant_postnatal: {
       '1.0': infant_postnatal_v_1_0,
-      antenatal: {
-        '1.0': antenatal_v_1_0,
-      },
     },
   },
 };
