ca86df768fe81364700cc7183ba319f541e60ae1
pirupius <piruville@gmail.com>
2023-05-31 12:34:26 +0300

    OHRI-1364 rename ohri dashboards to programs

--- packages/esm-ohri-core-app/src/ohri-dashboard/side-menu/ohri-dashboard-side-nav.component.tsx
+++ packages/esm-ohri-core-app/src/ohri-dashboard/side-menu/ohri-dashboard-side-nav.component.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { SideNav, SideNavItems, SideNavLink } from '@carbon/react';
+import { SideNav, SideNavItems } from '@carbon/react';
 import styles from './ohri-dashboard-side-nav.scss';
-import { ListBulleted, Calendar } from '@carbon/react/icons';
 import { ExtensionSlot } from '@openmrs/esm-framework';
 import { useTranslation } from 'react-i18next';
 
@@ -10,24 +9,11 @@ const OHRIDashboardSideNav = () => {
   return (
     <SideNav isFixedNav expanded={true} isChildOfHeader={false} aria-label="Side navigation" className={styles.sideNav}>
       <SideNavItems>
-        <ExtensionSlot extensionSlotName="dashboard-links-slot" />
+        <ExtensionSlot name="dashboard-links-slot" />
 
-        {/* Remove once we have dashboards */}
-        {/* <div>
-          <SideNavLink renderIcon={ListBulleted} href="javascript:void(0)">
-            {t('patientLists', 'Patient Lists')}
-          </SideNavLink>
-          <SideNavLink renderIcon={Calendar} href="javascript:void(0)">
-            {t('appointments', 'Appointments')}
-          </SideNavLink>
-          <SideNavLink renderIcon={Calendar} href="javascript:void(0)">
-            {t('appointments', 'Appointments')}
-          </SideNavLink>
-        </div> */}
+        <p className={styles.sideNavTextHeader}>{t('programs', 'Programs')}</p>
 
-        <p className={styles.sideNavTextHeader}>{t('myDashboards', 'My Dashboards')}</p>
-
-        <ExtensionSlot extensionSlotName="dashboard-slot" />
+        <ExtensionSlot name="dashboard-slot" />
       </SideNavItems>
     </SideNav>
   );
