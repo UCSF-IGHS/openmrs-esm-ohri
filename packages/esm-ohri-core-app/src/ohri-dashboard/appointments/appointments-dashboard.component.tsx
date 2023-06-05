9c9b07feb7d8d3213e516bcce214f12976efc38a
kajambiya <jssebaggala@outlook.com>
2023-06-05 16:25:55 +0300

    add fix for unmounting dashboards

--- packages/esm-ohri-core-app/src/ohri-dashboard/appointments/appointments-dashboard.component.tsx
+++ packages/esm-ohri-core-app/src/ohri-dashboard/appointments/appointments-dashboard.component.tsx
@@ -2,11 +2,6 @@ import React, { useEffect } from 'react';
 import { attach, detach, ExtensionSlot } from '@openmrs/esm-framework';
 
 const AppointmentsDashboard = () => {
-  useEffect(() => {
-    attach('ohri-dashboard-appointments-slot', 'clinical-appointments-dashboard');
-    return () => detach('ohri-dashboard-appointments-slot', 'clinical-appointments-dashboard');
-  }, []);
-
   return <ExtensionSlot extensionSlotName="ohri-dashboard-appointments-slot" state={{}} />;
 };
 
