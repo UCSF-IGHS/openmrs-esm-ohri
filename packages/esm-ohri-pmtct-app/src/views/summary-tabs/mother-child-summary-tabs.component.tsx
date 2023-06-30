0595525992431135866b160a73d95e4883f5d3ca
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-06-30 11:07:28 +0300

    OHRI-1544 Remove the Postnatal, Delivery and Antenatal tabs from the MNCH Clinical DB

--- packages/esm-ohri-pmtct-app/src/views/summary-tabs/mother-child-summary-tabs.component.tsx
+++ packages/esm-ohri-pmtct-app/src/views/summary-tabs/mother-child-summary-tabs.component.tsx
@@ -3,9 +3,6 @@ import { Tabs, Tab, TabPanels, TabPanel, TabList } from '@carbon/react';
 import styles from '../summary-tabs/ohri-patient-tabs.scss';
 import { useTranslation } from 'react-i18next';
 import AllClients from './all-clients.component';
-import AntentalList from './antenatal.component';
-import DeliveryList from './delivery.component';
-import PostnatalList from './postnatal.component';
 
 interface OverviewListProps {
   patientUuid: string;
@@ -17,22 +14,10 @@ const LabResultsSummary: React.FC<OverviewListProps> = ({ patientUuid }) => {
     <Tabs type="container" className={styles.tabContainer}>
       <TabList contained>
         <Tab>{t('allClients', 'All Clients')}</Tab>
-        <Tab>{t('antenatal', 'Antenatal')}</Tab>
-        <Tab>{t('delivery', 'Delivery')}</Tab>
-        <Tab>{t('postnatal', 'Postnatal')}</Tab>
       </TabList>
       <TabPanels>
         <TabPanel>
           <AllClients patientUuid={patientUuid} />
-        </TabPanel>
-        <TabPanel>
-          <AntentalList patientUuid={patientUuid} />
-        </TabPanel>
-        <TabPanel>
-          <DeliveryList patientUuid={patientUuid} />
-        </TabPanel>
-        <TabPanel>
-          <PostnatalList patientUuid={patientUuid} />
         </TabPanel>
       </TabPanels>
     </Tabs>
