87520ad7ae355b3f043663db827f65d0a61cce08
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-07-03 11:00:21 +0300

    Remove the All clients component

--- packages/esm-ohri-pmtct-app/src/views/summary-tabs/mother-child-summary-tabs.component.tsx
+++ packages/esm-ohri-pmtct-app/src/views/summary-tabs/mother-child-summary-tabs.component.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Tabs, Tab, TabPanels, TabPanel, TabList } from '@carbon/react';
 import styles from '../summary-tabs/ohri-patient-tabs.scss';
 import { useTranslation } from 'react-i18next';
-import AllClients from './all-clients.component';
 import { PatientListTable } from '@ohri/openmrs-esm-ohri-commons-lib';
 
 interface OverviewListProps {
