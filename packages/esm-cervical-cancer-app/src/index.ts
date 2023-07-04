aa68d1fbd5da6bac67f52b9c38969831172bd34f
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-07-04 15:55:28 +0300

    OHRI-1581 Migrate CaCx

--- packages/esm-cervical-cancer-app/src/index.ts
+++ packages/esm-cervical-cancer-app/src/index.ts
@@ -8,82 +8,43 @@ import {
 } from './dashboard.meta';
 import { createDashboardGroup, createDashboardLink } from '@openmrs/esm-patient-common-lib';
 
-const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
 require('./root.scss');
 
 export const moduleName = '@ohri/openmrs-esm-ohri-cervical-cancer-app';
 
-function setupOpenMRS() {
-  const options = {
-    featureName: 'ohri-cervical-cancer',
-    moduleName,
-  };
+const options = {
+  featureName: 'ohri-cervical-cancer',
+  moduleName,
+};
 
+function startupApp() {
   defineConfigSchema(moduleName, {});
-
-  return {
-    pages: [],
-    extensions: [
-      {
-        id: 'cervical-cancer',
-        slot: 'patient-chart-dashboard-slot',
-        load: getSyncLifecycle(createDashboardGroup(cervicalCancerFolderMeta), options),
-        meta: cervicalCancerFolderMeta,
-        online: true,
-        offline: true,
-        order: 24,
-      },
-      {
-        id: 'cacx-summary-dashboard',
-        slot: 'cervical-cancer-slot',
-        load: getSyncLifecycle(createDashboardLink(caCxSummaryDashboardMeta), options),
-        meta: caCxSummaryDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'cacx-summary-ext',
-        slot: 'cacx-summary-slot',
-        load: getAsyncLifecycle(() => import('./views/cacx-summary/cacx-summary.component'), {
-          featureName: 'cacx-summary',
-          moduleName,
-        }),
-      },
-      {
-        id: 'cacx-visits-dashboard',
-        slot: 'cervical-cancer-slot',
-        load: getSyncLifecycle(createDashboardLink(caCxVisitsDashboardMeta), options),
-        meta: caCxVisitsDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'cacx-visits-ext',
-        slot: 'cacx-visits-slot',
-        load: getAsyncLifecycle(() => import('./views/cacx-visits/cacx-visits-services.component'), {
-          featureName: 'cacx-visits',
-          moduleName,
-        }),
-      },
-      {
-        id: 'cacx-appointments-dashboard',
-        slot: 'cervical-cancer-slot',
-        load: getSyncLifecycle(createDashboardLink(caCxAppointmentsDashboardMeta), options),
-        meta: caCxAppointmentsDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'cacx-appointments-ext',
-        slot: 'cacx-appointments-slot',
-        load: getAsyncLifecycle(() => import('./views/cacx-appointment/cacx-appointments.component'), {
-          featureName: 'cacx-appointments',
-          moduleName,
-        }),
-      },
-    ],
-  };
 }
 
-export { backendDependencies, importTranslation, setupOpenMRS };
+export const cacxDashboard = getSyncLifecycle(createDashboardGroup(cervicalCancerFolderMeta), options);
+export const cacxSummaryDashboardLink = getSyncLifecycle(createDashboardLink(caCxSummaryDashboardMeta), options);
+export const cacxSummaryDashboard = getAsyncLifecycle(() => import('./views/cacx-summary/cacx-summary.component'), {
+  featureName: 'cacx-summary',
+  moduleName,
+});
+export const cacxVisitsDashboardLink = getSyncLifecycle(createDashboardLink(caCxVisitsDashboardMeta), options);
+export const cervicalCancerVisitsDashboard = getAsyncLifecycle(
+  () => import('./views/cacx-visits/cacx-visits-services.component'),
+  {
+    featureName: 'cacx-visits',
+    moduleName,
+  },
+);
+export const cervicalCancerDashboardLink = getSyncLifecycle(
+  createDashboardLink(caCxAppointmentsDashboardMeta),
+  options,
+);
+export const cacxAppointmentDashboard = getAsyncLifecycle(
+  () => import('./views/cacx-appointment/cacx-appointments.component'),
+  {
+    featureName: 'cacx-appointments',
+    moduleName,
+  },
+);
