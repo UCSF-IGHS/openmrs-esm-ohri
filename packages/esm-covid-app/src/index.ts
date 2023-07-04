67f24275208f5d07a6893f7e61a6cd8c5d01591b
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-07-04 15:09:09 +0300

    OHRI-1578 Migrate OHRI

--- packages/esm-covid-app/src/index.ts
+++ packages/esm-covid-app/src/index.ts
@@ -11,141 +11,72 @@ import {
 import { createOHRIDashboardLink, OHRIHome, OHRIWelcomeSection } from '@ohri/openmrs-esm-ohri-commons-lib';
 import { createDashboardGroup, createDashboardLink } from '@openmrs/esm-patient-common-lib';
 
-const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
 export const moduleName = '@ohri/openmrs-esm-ohri-covid-app';
 
 require('./root.scss');
 
-function setupOpenMRS() {
-  const options = {
-    featureName: 'ohri-covid',
-    moduleName,
-  };
+const options = {
+  featureName: 'ohri-covid',
+  moduleName,
+};
 
+export function startupApp() {
   defineConfigSchema(moduleName, {});
-  return {
-    pages: [],
-    extensions: [
-      {
-        id: 'covid-home-header-ext',
-        slot: 'covid-home-header-slot',
-        load: getSyncLifecycle(OHRIWelcomeSection, {
-          featureName: 'covid-home-header',
-          moduleName,
-        }),
-      },
-      {
-        id: 'covid-home-tile-ext',
-        slot: 'covid-home-tiles-slot',
-        load: getAsyncLifecycle(() => import('./views/dashboard/summary-tiles/covid-summary-tiles.component'), {
-          featureName: 'covid-home-tiles',
-          moduleName,
-        }),
-      },
-      {
-        id: 'covid-home-tabs-ext',
-        slot: 'covid-home-tabs-slot',
-        load: getAsyncLifecycle(() => import('./views/dashboard/patient-list-tabs/covid-patient-list-tabs.component'), {
-          featureName: 'covid-home-tabs',
-          moduleName,
-        }),
-      },
-      {
-        id: 'covid-dashboard-items',
-        slot: 'dashboard-slot',
-        load: getSyncLifecycle(createOHRIDashboardLink(covidClinicalViewDashboardMeta), options),
-        meta: covidClinicalViewDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'covid-cases-dashboard-ext',
-        slot: 'ohri-covid-dashboard-slot',
-        load: getSyncLifecycle(createOHRIDashboardLink(covid19CasesDashboardMeta), options),
-        meta: covid19CasesDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'covid-cases-dashboard',
-        slot: 'covid-cases-dashboard-slot',
-        load: getSyncLifecycle(OHRIHome, {
-          featureName: 'covid cases dashboard',
-          moduleName,
-        }),
-        meta: covid19CasesDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'ohri-covid',
-        slot: 'patient-chart-dashboard-slot',
-        load: getSyncLifecycle(createDashboardGroup(covidPatientChartMeta), options),
-        meta: covidPatientChartMeta,
-        online: true,
-        offline: true,
-        order: 23,
-      },
-      {
-        id: 'covid-assessments-dashboard',
-        slot: 'ohri-covid-patient-chart-slot',
-        load: getSyncLifecycle(createDashboardLink(covidAssessmentsDashboardMeta), options),
-        meta: covidAssessmentsDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'covid-lab-results',
-        slot: 'ohri-covid-patient-chart-slot',
-        load: getSyncLifecycle(createDashboardLink(covidLabTestsDashboardMeta), options),
-        meta: covidLabTestsDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'covid-vaccinations-dashboard',
-        slot: 'ohri-covid-patient-chart-slot',
-        load: getSyncLifecycle(createDashboardLink(covidVaccinationsDashboardMeta), options),
-        meta: covidVaccinationsDashboardMeta,
-        online: true,
-        offline: true,
-      },
-      {
-        id: 'covid-assessments-ext',
-        slot: 'covid-assessments-dashboard-slot',
-        load: getAsyncLifecycle(() => import('./views/case-assessment.encounter-lists'), {
-          featureName: 'covid-assessment',
-          moduleName,
-        }),
-        meta: {
-          columnSpan: 4,
-        },
-      },
-      {
-        id: 'covid-Lab-results-ext',
-        slot: 'covid-lab-dashboard-slot',
-        load: getAsyncLifecycle(() => import('./views/lab-results.encounter-list'), {
-          featureName: 'covid-lab-results',
-          moduleName,
-        }),
-        meta: {
-          columnSpan: 4,
-        },
-      },
-      {
-        id: 'covid-vaccinations-ext',
-        slot: 'covid-vaccinations-dashboard-slot',
-        load: getAsyncLifecycle(() => import('./views/covid-vaccinations.encounter-list'), {
-          featureName: 'covid-vaccinations',
-          moduleName,
-        }),
-        meta: {
-          columnSpan: 4,
-        },
-      },
-    ],
-  };
 }
 
-export { backendDependencies, importTranslation, setupOpenMRS };
+export const covidDashboard = getSyncLifecycle(createDashboardGroup(covidPatientChartMeta), options);
+export const covidSummaryDashboardLink = getSyncLifecycle(
+  createOHRIDashboardLink(covidClinicalViewDashboardMeta),
+  options,
+);
+export const covidAssessmentDashboardLink = getSyncLifecycle(
+  createDashboardLink(covidAssessmentsDashboardMeta),
+  options,
+);
+export const covidVaccinationDashboardLink = getSyncLifecycle(
+  createDashboardLink(covidVaccinationsDashboardMeta),
+  options,
+);
+export const covidPatientChartDashboardLink = getSyncLifecycle(createOHRIDashboardLink(covidPatientChartMeta), options);
+export const covidDashboardItemsLink = getSyncLifecycle(
+  createOHRIDashboardLink(covidClinicalViewDashboardMeta),
+  options,
+);
+export const covidLabResultsDashboardLink = getSyncLifecycle(createDashboardLink(covidLabTestsDashboardMeta), options);
+export const covidDashboardHeader = getSyncLifecycle(OHRIWelcomeSection, {
+  featureName: 'covid-home-header',
+  moduleName,
+});
+export const covidDashboardTiles = getAsyncLifecycle(
+  () => import('./views/dashboard/summary-tiles/covid-summary-tiles.component'),
+  {
+    featureName: 'covid-home-tiles',
+    moduleName,
+  },
+);
+export const covidDashboardTabs = getAsyncLifecycle(
+  () => import('./views/dashboard/patient-list-tabs/covid-patient-list-tabs.component'),
+  {
+    featureName: 'covid-home-tabs',
+    moduleName,
+  },
+);
+export const covidAssessmentsDashboard = getAsyncLifecycle(() => import('./views/case-assessment.encounter-lists'), {
+  featureName: 'covid-assessment',
+  moduleName,
+});
+export const covidLabResultsDashboard = getAsyncLifecycle(() => import('./views/lab-results.encounter-list'), {
+  featureName: 'covid-lab-results',
+  moduleName,
+});
+export const covidVaccinationsDashboard = getAsyncLifecycle(() => import('./views/covid-vaccinations.encounter-list'), {
+  featureName: 'covid-vaccinations',
+  moduleName,
+});
+export const covidCasesDashboardLink = getSyncLifecycle(createOHRIDashboardLink(covid19CasesDashboardMeta), options);
+export const covidCasesDashboard = getSyncLifecycle(OHRIHome, {
+  featureName: 'covid cases dashboard',
+  moduleName,
+});
