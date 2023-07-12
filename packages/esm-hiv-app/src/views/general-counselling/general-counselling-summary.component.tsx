2f2d5a6c418da8857f6152c555231e076fa4413c
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-07-12 13:54:18 +0300

    OHRI-1637 Remove drug and alcohol use tab

--- packages/esm-hiv-app/src/views/general-counselling/general-counselling-summary.component.tsx
+++ packages/esm-hiv-app/src/views/general-counselling/general-counselling-summary.component.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Tabs, Tab, TabList, TabPanels, TabPanel } from '@carbon/react';
 import styles from '../common.scss';
 import MentalHealthAssessmentList from './tabs/mental-health-assessment.component';
-import DrugsAndAlcoholUseList from './tabs/drugs-and-alcohol-use.component';
 import IntimatePartnerViolenceList from './tabs/intimate-partner-violence.component';
 import DisclosureList from './tabs/disclosure.component';
 import { useTranslation } from 'react-i18next';
@@ -19,16 +18,12 @@ const GeneralCounsellingSummary: React.FC<OverviewListProps> = ({ patientUuid })
       <Tabs>
         <TabList contained>
           <Tab className="tab-14rem">{t('mentalHealthAssessment', 'Mental Health Assessment')}</Tab>
-          <Tab className="tab-12rem">{t('drugsAndAlcoholUse', 'Drugs and Alcohol Use')}</Tab>
           <Tab className="tab-14rem">{t('intimatePartnerViolence', 'Intimate Partner Violence')}</Tab>
           <Tab>{t('disclosure', 'Disclosure')}</Tab>
         </TabList>
         <TabPanels>
           <TabPanel>
             <MentalHealthAssessmentList patientUuid={patientUuid} />
-          </TabPanel>
-          <TabPanel>
-            <DrugsAndAlcoholUseList patientUuid={patientUuid} />
           </TabPanel>
           <TabPanel>
             <IntimatePartnerViolenceList patientUuid={patientUuid} />
