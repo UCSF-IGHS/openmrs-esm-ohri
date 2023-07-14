4110e0c1d5bb01b42ea6b2f29eaba151fbaddb9a
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-07-14 08:57:24 +0300

    OHRI-1657 Remove TB Screening tab (#1575)
    * OHRI-1657 Remove TB Screening tab
    
    * Remove TB Screening component

--- packages/esm-cervical-cancer-app/src/views/cacx-visits/cacx-visits-services.component.tsx
+++ packages/esm-cervical-cancer-app/src/views/cacx-visits/cacx-visits-services.component.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Tabs, Tab, TabList, TabPanels, TabPanel } from '@carbon/react';
 import styles from '../common.scss';
 import CaCxRegistrationList from './Tabs/cacx-registration.component';
-import CacxScreeningList from './Tabs/cacx-screening.component';
 import CacxTreatmentList from './Tabs/cacx-treatment.component';
 import { useTranslation } from 'react-i18next';
 
@@ -18,15 +17,11 @@ const CaCxCervicalCancerServices: React.FC<OverviewListProps> = ({ patientUuid }
       <Tabs>
         <TabList contained>
           <Tab className="tab-12rem">{t('cacxRegistration', 'Cacx Registration')}</Tab>
-          <Tab>{t('cacxScreening', 'CaCx Screening')}</Tab>
           <Tab>{t('cacxTreatment', 'CaCx Treatment')}</Tab>
         </TabList>
         <TabPanels>
           <TabPanel>
             <CaCxRegistrationList patientUuid={patientUuid} />
-          </TabPanel>
-          <TabPanel>
-            <CacxScreeningList patientUuid={patientUuid} />
           </TabPanel>
           <TabPanel>
             <CacxTreatmentList patientUuid={patientUuid} />
