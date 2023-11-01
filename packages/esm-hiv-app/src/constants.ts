58e7b816ddedd13fad6664ebd26f94ffaeece001
dennistaiti <dennistaiti7@gmail.com>
2023-11-01 10:01:48 +0300

    new changes

--- packages/esm-hiv-app/src/constants.ts
+++ packages/esm-hiv-app/src/constants.ts
@@ -44,7 +44,7 @@ export const dateOfHIVDiagnosisConcept = '160554AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 export const entryPointConcept = '160540AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 export const re_enrolmentDateConcept = '20efadf9-86d3-4498-b3ab-7da4dad9c429';
 export const otherEntryPoint = '161011AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
-export const HIVEnrolmentFormName = 'HIV Enrolment Form v1.1';
+export const HIVEnrolmentFormName = '746737c0-4201-4d7f-b580-816e36dc425a';
 
 // Clinical visit
 export const ClinicalVisitFormName = 'POC Clinical Visit Form v2';
