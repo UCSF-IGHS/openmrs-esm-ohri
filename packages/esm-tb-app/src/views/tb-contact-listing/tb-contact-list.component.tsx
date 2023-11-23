f38eda1426ab0c4afa49cc4e1882a9afcb82c242
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-11-23 12:53:26 +0300

    OHRI-1999 Add Elicited Contacts table under the TB Contact Listing menu

--- packages/esm-tb-app/src/views/tb-contact-listing/tb-contact-list.component.tsx
+++ packages/esm-tb-app/src/views/tb-contact-listing/tb-contact-list.component.tsx
@@ -1,15 +1,71 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { EmptyStateComingSoon, PatientChartProps } from '@ohri/openmrs-esm-ohri-commons-lib';
+import {
+  EncounterList,
+  EncounterListColumn,
+  PatientChartProps,
+  getObsFromEncounter,
+} from '@ohri/openmrs-esm-ohri-commons-lib';
+import { moduleName } from '../../index';
 
 const TbContactTracingList: React.FC<PatientChartProps> = ({ patientUuid }) => {
   const { t } = useTranslation();
   const headerTitle = t('TbContactListing', 'TB Contact Listing');
 
+  const columns: EncounterListColumn[] = useMemo(
+    () => [
+      {
+        key: 'caseID',
+        header: t('caseId', 'Case ID'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, ' ');
+        },
+      },
+      {
+        key: 'nameOfContact',
+        header: t('nameOfContact', 'Name of Contact'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '', true);
+        },
+      },
+      {
+        key: 'relationshipToIndex',
+        header: t('relationshipToIndex', 'Relationship to Index'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '');
+        },
+      },
+      {
+        key: 'phoneNumber',
+        header: t('phoneNumber', 'Phone Number'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '', true);
+        },
+      },
+      {
+        key: 'address',
+        header: t('address', 'Address'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '', true);
+        },
+      },
+    ],
+    [],
+  );
+
   return (
-    <>
-      <EmptyStateComingSoon displayText={headerTitle} headerTitle={headerTitle} />
-    </>
+    <EncounterList
+      patientUuid={patientUuid}
+      encounterType={''}
+      formList={[{ name: '' }]}
+      columns={columns}
+      description={headerTitle}
+      headerTitle={headerTitle}
+      launchOptions={{
+        displayText: t('add', 'Add'),
+        moduleName: moduleName,
+      }}
+    />
   );
 };
 
