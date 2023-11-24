a7001e111d90cb22c8fd1504c6a162f83f3a2a8d
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-11-24 08:14:04 +0300

    OHRI-1996 Patient Tracing table under TB program management menu

--- packages/esm-tb-app/src/views/program-management/tabs/tb-patient-tracing.component.tsx
+++ packages/esm-tb-app/src/views/program-management/tabs/tb-patient-tracing.component.tsx
@@ -1,15 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { EmptyStateComingSoon, PatientChartProps } from '@ohri/openmrs-esm-ohri-commons-lib';
+import {
+  EncounterList,
+  EncounterListColumn,
+  PatientChartProps,
+  getObsFromEncounter,
+} from '@ohri/openmrs-esm-ohri-commons-lib';
+import { moduleName } from '../../..';
 
 const TbPatientTracing: React.FC<PatientChartProps> = ({ patientUuid }) => {
   const { t } = useTranslation();
   const headerTitle = t('PatientTracing', 'Patient Tracing');
+  const columns: EncounterListColumn[] = useMemo(
+    () => [
+      {
+        key: 'contactDate',
+        header: t('contactDate', 'Contact Date'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '', true);
+        },
+      },
+      {
+        key: 'contactMethod',
+        header: t('contactMethod', 'Contact Method'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '');
+        },
+      },
+      {
+        key: 'contactOutcome',
+        header: t('contactOutcome', 'Contact Outcome'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '', true);
+        },
+      },
+    ],
+    [],
+  );
 
   return (
-    <>
-      <EmptyStateComingSoon displayText={headerTitle} headerTitle={headerTitle} />
-    </>
+    <EncounterList
+      patientUuid={patientUuid}
+      encounterType={''}
+      formList={[{ name: '' }]}
+      columns={columns}
+      description={headerTitle}
+      headerTitle={headerTitle}
+      launchOptions={{
+        displayText: t('add', 'Add'),
+        moduleName: moduleName,
+      }}
+    />
   );
 };
 
