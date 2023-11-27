bc81a3df9a5f4255b71a3a60be070d05cfcb8248
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-11-27 19:50:41 +0300

    OHRI-1995 MDR TB Enrolment table under TB program management menu

--- packages/esm-tb-app/src/views/program-management/tabs/mdr-tb.component.tsx
+++ packages/esm-tb-app/src/views/program-management/tabs/mdr-tb.component.tsx
@@ -1,15 +1,84 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { EmptyStateComingSoon, PatientChartProps } from '@ohri/openmrs-esm-ohri-commons-lib';
+import {
+  EncounterList,
+  EncounterListColumn,
+  PatientChartProps,
+  getObsFromEncounter,
+} from '@ohri/openmrs-esm-ohri-commons-lib';
+import { moduleName } from '../../..';
 
 const MdrTbList: React.FC<PatientChartProps> = ({ patientUuid }) => {
   const { t } = useTranslation();
   const headerTitle = t('MdrTbEnrolment', 'TB/MDR TB Enrolment');
+  const columns: EncounterListColumn[] = useMemo(
+    () => [
+      {
+        key: 'contactDate',
+        header: t('contactDate', 'Enrollment Date'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '', true);
+        },
+      },
+      {
+        key: 'contactMethod',
+        header: t('contactMethod', 'Case ID'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '');
+        },
+      },
+      {
+        key: 'contactOutcome',
+        header: t('contactOutcome', 'TB Treatement ID'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '');
+        },
+      },
+      {
+        key: 'contactOutcome',
+        header: t('contactOutcome', 'Treatment Start Date'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '', true);
+        },
+      },
+      {
+        key: 'contactOutcome',
+        header: t('contactOutcome', 'Regimen'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '');
+        },
+      },
+      {
+        key: 'contactOutcome',
+        header: t('contactOutcome', 'Treatment outcome'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '');
+        },
+      },
+      {
+        key: 'contactOutcome',
+        header: t('contactOutcome', 'Date of Treatment Outcome'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, '', true);
+        },
+      },
+    ],
+    [],
+  );
 
   return (
-    <>
-      <EmptyStateComingSoon displayText={headerTitle} headerTitle={headerTitle} />
-    </>
+    <EncounterList
+      patientUuid={patientUuid}
+      encounterType={''}
+      formList={[{ name: '' }]}
+      columns={columns}
+      description={headerTitle}
+      headerTitle={headerTitle}
+      launchOptions={{
+        displayText: t('add', 'Add'),
+        moduleName: moduleName,
+      }}
+    />
   );
 };
 
