0c113b90ded19ee711342aedae5df75895704c11
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-11-30 14:59:52 +0300

    OHRI-1999 Elicited Contacts table under the TB Contact Listing menu

--- packages/esm-tb-app/src/constants.ts
+++ packages/esm-tb-app/src/constants.ts
@@ -1,3 +1,9 @@
+//TB Patient Tracing
 export const TbPatientTracingEncounterType = '98c938e9-fb3e-4982-ae22-0305cbd12f8c';
 export const contactDate = '160753AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 export const contactMethod = '166456AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+
+//TB Client Listing
+export const ClientListingEncounterType = '70d2b2f6-860b-438c-994b-c28e863dca34';
+export const CaseId = '162576AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+export const DateContacted = '160753AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
