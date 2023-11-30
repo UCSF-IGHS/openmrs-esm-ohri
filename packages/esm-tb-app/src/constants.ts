d9f01b687dc36ca92f607e61efd0b5bb7a4278d7
lucyjemutai <lucyjemutai@uonbi.ac.ke>
2023-11-30 03:27:40 -0800

    Add All TB Clients Table in the Tuberculosis Treatment dashboard

--- packages/esm-tb-app/src/constants.ts
+++ packages/esm-tb-app/src/constants.ts
@@ -1,3 +1,14 @@
 export const TbPatientTracingEncounterType = '98c938e9-fb3e-4982-ae22-0305cbd12f8c';
 export const contactDate = '160753AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 export const contactMethod = '166456AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+// COVID Cohorts
+export const clientsAssessedForCovid = 'ec373b01-4ba3-488e-a322-9dd6a50cfdf7';
+
+// TB Encounter types
+export const tbCaseEnrollmentEncType ='9a199b59-b185-485b-b9b3-a9754e65ae57';
+
+// TB Concepts
+export const tbOutcome ='159786AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+export const typeOfTb ='160040AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+export const drugSensitivity ='163775AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+export const caseId ='162576AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
--- packages/esm-tb-app/src/views/dashboard/patient-list-tabs/tb-patient-list-tabs.component.tsx
+++ packages/esm-tb-app/src/views/dashboard/patient-list-tabs/tb-patient-list-tabs.component.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import { Tabs, Tab, TabPanels, TabPanel, TabList } from '@carbon/react';
+import React, { useMemo } from 'react';
+import {
+  tbCaseEnrollmentEncType,
+  caseId,
+  drugSensitivity,
+  typeOfTb,
+  tbOutcome,
+  clientsAssessedForCovid,
+
+} from '../../../constants';
 import { useTranslation } from 'react-i18next';
+import {
+  getObsFromEncounter,
+  OHRIPatientListTabs,
+} from '@ohri/openmrs-esm-ohri-commons-lib';
+import moment from 'moment';
+import { moduleName } from '../../../index';
 
-interface OverviewListProps {
-  patientUuid: string;
-}
-
-const TbHomePatientTabs: React.FC<OverviewListProps> = ({ patientUuid }) => {
+function TbHomePatientTabs() {
   const { t } = useTranslation();
-  return (
-    <Tabs >
-      <TabList contained>
-        <Tab>{t('allTbClients', 'All TB Clients')}</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+
+  const tabsConfigs = useMemo(
+    () => [
+      {
+        label: t('allTbClients', 'All TB Clients'),
+        cohortId: clientsAssessedForCovid,
+        isReportingCohort: true,
+        cohortSlotName: 'clients-assessed-for-covid-slot',
+        launchableForm: {
+          editActionText: t('editAssessmentForm', 'Edit TB case assessment form'),
+          editLatestEncounter: true,
+          targetDashboard: 'tb-assessments',
+          name: 'TB Case Enrollment Form',
+        },
+        associatedEncounterType: tbCaseEnrollmentEncType,
+        excludeColumns: ['timeAddedToList', 'waitingTime', 'location', 'phoneNumber', 'hivResult'],
+        otherColumns: [
+          {
+            key: 'caseId',
+            header: t('caseId', 'Case ID'),
+            getValue: ({ latestEncounter }) => {
+              return getObsFromEncounter(latestEncounter, caseId);
+            },
+            index: 1,
+          },
+          {
+            key: 'drugSensitivity',
+            header: t('drugSensitivity', 'Drug Sensitivity'),
+            getValue: ({ latestEncounter }) => {
+              return getObsFromEncounter(latestEncounter, drugSensitivity);
+            },
+          },
+          {
+            key: 'typeTb',
+            header: t('typeTB', 'Type of TB'),
+            getValue: ({ latestEncounter }) => {
+              return getObsFromEncounter(latestEncounter, typeOfTb);
+            },
+          },
+          {
+            key: 'appointmentDate',
+            header: t('appointmentDate', 'Appointment Date'),
+            getValue: ({ latestEncounter }) => {
+              return latestEncounter && moment(latestEncounter.encounterDatetime).format('DD-MMM-YYYY');
+            },
+          },
+          {
+            key: 'outcome',
+            header: t('outcome', 'Outcome'),
+            getValue: ({ latestEncounter }) => {
+              return getObsFromEncounter(latestEncounter, tbOutcome);
+            },
+          },
+        ],
+      },
+    ],
+    [],
   );
-};
+  return <OHRIPatientListTabs patientListConfigs={tabsConfigs} moduleName={moduleName} />;
+}
 
 export default TbHomePatientTabs;
