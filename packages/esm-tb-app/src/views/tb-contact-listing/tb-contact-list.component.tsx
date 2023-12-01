ed748fd69cbfb20fc9d84441ceed64b484967b90
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-12-01 09:13:49 +0300

    OHRI-1999 Elicited Contacts table under the TB Contact Listing menu (#1709)
    * OHRI-1999 Elicited Contacts table under the TB Contact Listing menu
    
    * Change translation keys

--- packages/esm-tb-app/src/views/tb-contact-listing/tb-contact-list.component.tsx
+++ packages/esm-tb-app/src/views/tb-contact-listing/tb-contact-list.component.tsx
@@ -1,15 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { EmptyStateComingSoon, PatientChartProps } from '@ohri/openmrs-esm-ohri-commons-lib';
+import {
+  EncounterList,
+  EncounterListColumn,
+  PatientChartProps,
+  getObsFromEncounter,
+} from '@ohri/openmrs-esm-ohri-commons-lib';
+import { moduleName } from '../../index';
+import { CaseId, ClientListingEncounterType, DateContacted } from '../../constants';
 
 const TbContactTracingList: React.FC<PatientChartProps> = ({ patientUuid }) => {
   const { t } = useTranslation();
   const headerTitle = t('TbContactListing', 'TB Contact Listing');
 
+  const columns: EncounterListColumn[] = useMemo(
+    () => [
+      {
+        key: 'caseID',
+        header: t('caseId', 'TB Case ID'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, CaseId);
+        },
+      },
+      {
+        key: 'dateContactListed',
+        header: t('dateContactListed', 'Date Contact Listed'),
+        getValue: (encounter) => {
+          return getObsFromEncounter(encounter, DateContacted, true);
+        },
+      },
+      {
+        key: 'location',
+        header: t('loction', 'Location'),
+        getValue: (encounter) => {
+          return encounter.location.name;
+        },
+      },
+      {
+        key: 'actions',
+        header: t('actions', 'Actions'),
+        getValue: (encounter) => [
+          {
+            form: { name: 'TB Contact Listing' },
+            encounterUuid: encounter.uuid,
+            intent: '*',
+            label: t('viewDetails', 'View Details'),
+            mode: 'view',
+          },
+          {
+            form: { name: 'TB Contact Listing' },
+            encounterUuid: encounter.uuid,
+            intent: '*',
+            label: t('editForm', 'Edit Form'),
+            mode: 'edit',
+          },
+        ],
+      },
+    ],
+    [],
+  );
+
   return (
-    <>
-      <EmptyStateComingSoon displayText={headerTitle} headerTitle={headerTitle} />
-    </>
+    <EncounterList
+      patientUuid={patientUuid}
+      encounterType={ClientListingEncounterType}
+      formList={[{ name: 'TB Contact Listing' }]}
+      columns={columns}
+      description={headerTitle}
+      headerTitle={headerTitle}
+      launchOptions={{
+        displayText: t('add', 'Add'),
+        moduleName: moduleName,
+      }}
+    />
   );
 };
 
