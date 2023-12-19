79100705527855b088313b85bc7ec7b77f22e92f
lucyjemutai <130601439+lucyjemutai@users.noreply.github.com>
2023-12-19 19:29:13 +0300

    OHRI-2037: Connect the TB dashboard tiles with the mamba reports (#1725)
    * Connect the TB dashboard tiles with the mamba reports
    
    * updated
    
    * updated
    
    * Update
    
    * Update tb-summary-tiles.component.tsx
    
    * Update mambaReport.test.ts
    
    * Update mambaReport.test.ts
    
    ---------
    
    Co-authored-by: lucyjemutai <lucyjemutai@uonbi.ac.ke>

--- packages/esm-tb-app/src/views/dashboard/summary-tiles/tb-summary-tiles.component.tsx
+++ packages/esm-tb-app/src/views/dashboard/summary-tiles/tb-summary-tiles.component.tsx
@@ -1,11 +1,31 @@
-import { OHRIProgrammeSummaryTiles } from '@ohri/openmrs-esm-ohri-commons-lib';
+import { OHRIProgrammeSummaryTiles, fetchMambaReportData } from '@ohri/openmrs-esm-ohri-commons-lib';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 function TbSummaryTiles({ launchWorkSpace }) {
   const { t } = useTranslation();
-  const [activeDSClientsCount, setActiveDSClientsCount] = useState(35);
-  const [activeDRClientsCount, setActiveDRClientsCount] = useState(5);
+  const [activeDSClientsCount, setActiveDSClientsCount] = useState(null);
+  const [activeDRClientsCount, setActiveDRClientsCount] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [dsCount, drCount] = await Promise.all([
+          fetchMambaReportData('total_active_ds_cases'),
+          fetchMambaReportData('total_active_dr_cases'),
+        ]);
+
+        setActiveDSClientsCount(dsCount);
+        setActiveDRClientsCount(drCount);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        throw new Error('Error fetching data. Please try again.');
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const tiles = useMemo(
     () => [
       {
@@ -21,8 +41,9 @@ function TbSummaryTiles({ launchWorkSpace }) {
         value: activeDRClientsCount,
       }
     ],
-    [],
+    [activeDSClientsCount, activeDRClientsCount],
   );
+
   return <OHRIProgrammeSummaryTiles tiles={tiles} />;
 }
 
