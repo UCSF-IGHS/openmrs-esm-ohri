328c110ce07d5612056c8d65b0af7ef8e43ef7b2
lucyjemutai <130601439+lucyjemutai@users.noreply.github.com>
2024-01-09 17:44:12 +0300

    Update current-ptracker-action.ts

--- packages/esm-ohri-pmtct-app/src/post-submission-actions/current-ptracker-action.ts
+++ packages/esm-ohri-pmtct-app/src/post-submission-actions/current-ptracker-action.ts
@@ -18,12 +18,12 @@ export const PTrackerSubmissionAction: PostSubmissionAction = {
 
 export async function updatePatientPtracker(encounter, encounterLocation, patientUuid) {
   const config = await getConfig('@ohri/openmrs-esm-ohri-pmtct');
-  const inComingPTrackerID = encounter.obs.find((observation) => observation.concept.uuid === config.encounterTypes.pTrackerIdConcept)?.value;
+  const inComingPTrackerID = encounter.obs.find((observation) => observation.concept.uuid === config.obsConcepts.pTrackerIdConcept)?.value;
   if (!inComingPTrackerID) {
     return;
   }
   const patientIdentifiers = await fetchPatientIdentifiers(patientUuid);
-  const existingPTrackers = patientIdentifiers.filter((id) => id.identifierType.uuid === config.encounterTypes.PTrackerIdentifierType);
+  const existingPTrackers = patientIdentifiers.filter((id) => id.identifierType.uuid === config.obsConcepts.PTrackerIdentifierType);
   if (existingPTrackers.some((ptracker) => ptracker.identifier === inComingPTrackerID)) {
     return;
   }
@@ -31,7 +31,7 @@ export async function updatePatientPtracker(encounter, encounterLocation, patien
   //add current ptracker to identities
   const currentPTrackerObject: PatientIdentifier = {
     identifier: inComingPTrackerID,
-    identifierType: config.PTrackerIdentifierType.PTrackerIdentifierType,
+    identifierType: config.obsConcepts.PTrackerIdentifierType,
     location: encounterLocation,
     preferred: false,
   };
