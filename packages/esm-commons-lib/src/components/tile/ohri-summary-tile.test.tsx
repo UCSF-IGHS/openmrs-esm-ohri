4fb8df82d6daaec285b418f3962ccc07e447596f
Jamie Arodi <51090527+arodidev@users.noreply.github.com>
2024-01-12 07:31:08 +0300

    OHRI-2086 Add unit tests for ohri summary tile in commons lib (#1756)
    * OHRI-2086 tests added
    
    * cleanup

---
+++ packages/esm-commons-lib/src/components/tile/ohri-summary-tile.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import { act, screen, render } from '@testing-library/react';
+import { OHRISummaryTile } from './ohri-summary-tile.component';
+
+const mockProps = {
+  title: 'Sample Title',
+  subTitle: 'Sample Subtitle',
+  value: 3077,
+  onClickView: () => null,
+};
+
+describe('ohri summary tiles', () => {
+  it('should display props on the UI', () => {
+    render(<OHRISummaryTile {...mockProps} />);
+
+    expect(screen.getByText(/Sample Title/i)).toBeInTheDocument();
+    expect(screen.getByText(/Sample Subtitle/i)).toBeInTheDocument();
+    expect(screen.getByText(/3077/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'View' })).toBeInTheDocument();
+  });
+});
