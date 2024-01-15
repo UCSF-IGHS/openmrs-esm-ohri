501577bbb8dc1b87f1d7dad6c8d3831222906630
lucyjemutai <130601439+lucyjemutai@users.noreply.github.com>
2024-01-16 00:34:14 +0300

    fix indents

--- packages/esm-commons-lib/src/components/tile/ohri-programme-summary-tiles.test.tsx
+++ packages/esm-commons-lib/src/components/tile/ohri-programme-summary-tiles.test.tsx
@@ -33,13 +33,11 @@ it('should handle the loading state', () => {
   const loadingProps = {
     tiles: [],
   };
-
   render(<OHRIProgrammeSummaryTiles {...loadingProps} />);
   const titleElement = screen.getByText('Programme summary');
   expect(titleElement).toBeInTheDocument();
   const subTitleElements = screen.queryAllByText(/People vaccinated/);
   const valueElements = screen.queryAllByText(/100/);
-
   expect(subTitleElements.length).toBe(0);
   expect(valueElements.length).toBe(0);
 });
