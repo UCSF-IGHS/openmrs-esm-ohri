45dc84e66ef57c0168039aef049135335a0432c5
lucyjemutai <130601439+lucyjemutai@users.noreply.github.com>
2024-01-15 15:14:20 +0300

    Add unit tests for tablet mode

--- packages/esm-commons-lib/src/components/tile/ohri-programme-summary-tiles.test.tsx
+++ packages/esm-commons-lib/src/components/tile/ohri-programme-summary-tiles.test.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { screen, render } from '@testing-library/react';
 import { OHRIProgrammeSummaryTiles } from './ohri-programme-summary-tiles.component';
+import { OHRISummaryTileTablet } from './ohri-summary-tile-tablet.component';
 
+// Mock data for OHRIProgrammeSummaryTiles
 const mockProps = {
   tiles: [
     {
@@ -15,6 +17,14 @@ const mockProps = {
   ],
 };
 
+// Mock data for OHRISummaryTileTablet
+const mockDetails = [
+  {
+    subTitle: 'Sample Subtitle',
+    value: 25,
+  },
+];
+
 describe('OHRIProgrammeSummaryTiles', () => {
   it('should display the summary tiles as expected', () => {
     render(<OHRIProgrammeSummaryTiles {...mockProps} />);
@@ -24,6 +34,20 @@ describe('OHRIProgrammeSummaryTiles', () => {
       expect(subTitleElements.length).toBeGreaterThan(0);
       const valueElements = screen.getAllByText(tile.value.toString());
       expect(valueElements.length).toBeGreaterThan(0);
+    });
+  });
+});
+
+describe('OHRISummaryTileTablet', () => {
+  it('should render the tablet summary tile', () => {
+    render(<OHRISummaryTileTablet details={mockDetails} />);
+
+    // Check if the title is rendered
+    expect(screen.getByText('Programme summary')).toBeInTheDocument();
+
+    mockDetails.forEach((detail) => {
+      expect(screen.getByText(detail.subTitle)).toBeInTheDocument();
+      expect(screen.getByText(detail.value.toString())).toBeInTheDocument();
     });
   });
 });
