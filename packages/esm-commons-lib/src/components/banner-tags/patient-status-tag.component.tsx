aa50af28cdbb2063a51ab668384d9b4f72ffce04
kirwea <117803438+kirwea@users.noreply.github.com>
2024-01-16 10:22:18 +0300

    OHRI 2018
    Added unit tests for patientStatus banner tag in commonsLib.

--- packages/esm-commons-lib/src/components/banner-tags/patient-status-tag.component.tsx
+++ packages/esm-commons-lib/src/components/banner-tags/patient-status-tag.component.tsx
@@ -1,36 +1,18 @@
-import React from 'react';
-import { render, act, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import { PatientStatusBannerTag } from './patient-status-tag.component';
+import React, { useEffect, useState } from 'react';
+import { Tag } from '@carbon/react';
+import { useTranslation } from 'react-i18next';
 import { isPatientHivPositive } from './patientHivStatus';
 
-const mockIsPatientHivPositive = isPatientHivPositive as jest.Mock;
-jest.mock('./patientHivStatus');
+export function PatientStatusBannerTag({ patientUuid }) {
+  const { t } = useTranslation();
+  const [hivPositive, setHivPositive] = useState(false);
 
-describe('PatientStatusBannerTag', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+  useEffect(() => {
+    isPatientHivPositive(patientUuid).then((result) => setHivPositive(result));
+  }, [hivPositive, patientUuid]);
 
-  const hivPositiveSampleUuid = '703AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+  //TODO: Improve refresh time
+  // forceRerender();
 
-  describe('PatientStatusBannerTag', () => {
-    it('renders red tag when patient is HIV positive', async () => {
-      mockIsPatientHivPositive.mockResolvedValue(true);
-      await act(async () => {
-        render(<PatientStatusBannerTag patientUuid={hivPositiveSampleUuid} />);
-      });
-
-      expect(screen.getByText(/HIV Positive/i)).toBeInTheDocument();
-    });
-  });
-
-  it('does not render red tag when patient is not HIV positive', async () => {
-    await act(async () => {
-      (isPatientHivPositive as jest.Mock).mockResolvedValue(false);
-      render(<PatientStatusBannerTag patientUuid="sampleUuid" />);
-    });
-
-    expect(screen.queryByText('HIV Positive')).not.toBeInTheDocument();
-  });
-});
+  return <>{hivPositive && <Tag type="red">{t('hivPositive', 'HIV Positive')}</Tag>}</>;
+}
