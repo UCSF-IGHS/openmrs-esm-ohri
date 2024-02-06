a1b5ff88b8b6d3049601e3a3ba0ee763a0828448
kirwea <117803438+kirwea@users.noreply.github.com>
2024-02-06 17:28:51 +0300

    OHRI-2124
    Added tptTreatment tab on the program management link board.

--- packages/esm-tb-app/src/views/tpt/program-management/tabs/tpt-treatment.component.tsx
+++ packages/esm-tb-app/src/views/tpt/program-management/tabs/tpt-treatment.component.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useConfig } from '@openmrs/esm-framework';
-import { EncounterList, getObsFromEncounter } from '@ohri/openmrs-esm-ohri-commons-lib';
+import { EncounterList, EncounterListColumn, getObsFromEncounter } from '@ohri/openmrs-esm-ohri-commons-lib';
 
 interface TptTreatmentListProps {
   patientUuid: string;
@@ -9,79 +9,68 @@ interface TptTreatmentListProps {
 
 const TptTreatmentList: React.FC<TptTreatmentListProps> = ({ patientUuid }) => {
   const { t } = useTranslation();
-  const { obsConcepts, encounterTypes } = useConfig();
+  const { obsConcepts, encounterTypes, formNames } = useConfig();
   const headerTitle = t('tptTreatment', 'TPT Treatment');
-  const columns = [
-    {
-      key: 'caseId',
-      header: t('caseId', 'Case Id'),
-      getValue: (encounter: any) => {
-        return getObsFromEncounter(encounter, obsConcepts.tptTreatmentId);
-      },
-    },
-    {
-      key: 'visitDate',
-      header: t('visitDate', 'Visit Date'),
-      getValue: (encounter: any) => {
-        return getObsFromEncounter(encounter, obsConcepts.followUpCaseId);
-      },
-    },
-    {
-      key: 'Regimen',
-      header: t('Regimen', 'Regimen'),
-      getValue: (encounter: any) => {
-        return getObsFromEncounter(encounter, obsConcepts.tptRegimen);
-      },
-    },
-    {
-      key: 'Adherence',
-      header: t('Adherence', 'Adherence'),
-      getValue: (encounter: any) => {
-        return getObsFromEncounter(encounter, obsConcepts.tptAdherence);
-      },
-    },
-    {
-      key: 'treatmentPlan',
-      header: t('treatmentPlan', 'Treatment Plan'),
-      getValue: (encounter: any) => {
-        return getObsFromEncounter(encounter, obsConcepts.treatmentPlan);
-      },
-    },
-    {
-      key: 'nextAppointment',
-      header: t('nextAppointment', 'Next Appointment'),
-      getValue: (encounter: any) => {
-        return getObsFromEncounter(encounter, obsConcepts.tptAppointmentDate);
-      },
-    },
-    {
-      key: 'actions',
-      header: t('actions', 'Actions'),
-      getValue: (encounter) => [
-        {
-          form: { name: 'TPT Case Enrolment form' },
-          encounterUuid: encounter.uuid,
-          intent: '*',
-          label: t('viewDetails', 'View Details'),
-          mode: 'view',
+  const columns: EncounterListColumn[] = useMemo(
+    () => [
+      {
+        key: 'caseId',
+        header: t('caseId', 'Case Id'),
+        getValue: (encounter: any) => {
+          return getObsFromEncounter(encounter, obsConcepts.caseID);
         },
-        {
-          form: { name: 'TPT Case Enrolment form' },
-          encounterUuid: encounter.uuid,
-          intent: '*',
-          label: t('editForm', 'Edit Form'),
-          mode: 'edit',
+      },
+      {
+        key: 'Adherence',
+        header: t('Adherence', 'Adherence'),
+        getValue: (encounter: any) => {
+          return getObsFromEncounter(encounter, obsConcepts.tptAdherence);
         },
-      ],
-    },
-  ];
+      },
+      {
+        key: 'treatmentPlan',
+        header: t('treatmentPlan', 'Treatment Plan'),
+        getValue: (encounter: any) => {
+          return getObsFromEncounter(encounter, obsConcepts.treatmentPlan);
+        },
+      },
+      {
+        key: 'nextAppointment',
+        header: t('nextAppointment', 'Next Appointment'),
+        getValue: (encounter: any) => {
+          return getObsFromEncounter(encounter, obsConcepts.tptAppointmentDate, true);
+        },
+      },
+      {
+        key: 'actions',
+        header: t('actions', 'Actions'),
+        getValue: (encounter) => [
+          {
+            form: { name: formNames.TptTreatmentFormName },
+            encounterUuid: encounter.uuid,
+            intent: '*',
+            label: t('viewDetails', 'View Details'),
+            mode: 'view',
+          },
+          {
+            form: { name: formNames.TptTreatmentFormName },
+            encounterUuid: encounter.uuid,
+            intent: '*',
+            label: t('editForm', 'Edit Form'),
+            mode: 'edit',
+          },
+        ],
+      },
+    ],
+    [],
+  );
   // eslint-disable-next-line no-empty-pattern
 
   return (
     <EncounterList
       patientUuid={patientUuid}
-      encounterType={encounterTypes.tptCaseEnrollment}
-      formList={[{ name: 'TPT Case Enrolment form' }]}
+      encounterType={encounterTypes.tptTreatmentAndFollowUp}
+      formList={[{ name: formNames.TptTreatmentFormName }]}
       description={headerTitle}
       headerTitle={headerTitle}
       columns={columns}
