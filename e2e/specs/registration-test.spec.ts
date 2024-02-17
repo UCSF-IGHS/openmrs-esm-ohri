3b00935a46a78498a49ea84807b2041cc37608de
lucyjemutai <130601439+lucyjemutai@users.noreply.github.com>
2024-02-17 13:37:40 +0300

    OHRI-2144: Setup e2e Tests in OHRI (#1773)
    * E2E test environment with playwright
    
    * .env file added
    
    * Rename ex.env to .env
    
    * Update test and rename register.spec.ts to registration.spec.ts
    
    * update the test
    
    * Ignore the e2e directory containing Playwright tests
    
    * GitHub Action to run your Playwright tests in the E2E Tests
    
    * including the e2e
    
    * updates
    
    * corrected YML file
    
    * update the yml
    
    * yml jobs
    
    * excluding the e2e in the jest config
    
    * yarn test-e2e
    
    * update the e2e test
    
    * yml
    
    * login.spec.ts
    
    * updates

---
+++ e2e/specs/registration-test.spec.ts
@@ -0,0 +1,40 @@
+import { test } from '../core';
+import { expect } from '@playwright/test';
+import { HomePage, ChartPage } from '../pages';
+import { type Patient, generateRandomPatient, startVisit } from '../commands';
+import { Visit } from '@openmrs/esm-framework';
+
+let patient: Patient; 
+let visit: Visit;
+
+test.beforeEach(async ({ api }) => {
+  patient = await generateRandomPatient(api);
+  visit = await startVisit(api, patient.uuid);
+});
+
+test('Go to homepage and register patient', async ({ page }) => {
+  const homePage = new HomePage(page);
+
+  await test.step('When I visit the home page', async () => {
+    await homePage.gotoHome();
+  });
+
+  await test.step('Then should be at the home page', async () => {
+    await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/dashboard/home`);
+  });
+
+  await page.getByLabel('Add Patient').click();
+  await page.getByLabel('First Name').click();
+  await page.getByLabel('First Name').fill('daisy');
+  await page.getByLabel('Family Name').click();
+  await page.getByLabel('Family Name').fill('daisy');
+  await page.getByText('Female').click();
+  await page.getByRole('tab', { name: 'No' }).nth(1).click();
+  await page.getByLabel('Estimated age in years').click();
+  await page.getByLabel('Estimated age in years').fill('20');
+
+  // Click on register patient button
+  
+  await page.getByRole('button', { name: 'Register Patient' }).click();
+  
+});
