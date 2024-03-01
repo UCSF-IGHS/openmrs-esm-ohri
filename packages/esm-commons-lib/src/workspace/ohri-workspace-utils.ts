903bd0a765f47756a8d040d85e2833d001012958
Jovan Ssebaggala <jssebaggala@outlook.com>
2024-03-01 16:57:34 +0300

    upgrade esm-patient-commons-lib, fix workspace issue (#1813)

--- packages/esm-commons-lib/src/workspace/ohri-workspace-utils.ts
+++ packages/esm-commons-lib/src/workspace/ohri-workspace-utils.ts
@@ -20,7 +20,7 @@ export const launchOHRIWorkSpace = (props: WorkspaceContextProps) => {
   const workspaceName = props.workspaceName || 'ohri-forms-' + counter++;
 
   const close = () => {
-    return closeWorkspace(workspaceName, true);
+    return closeWorkspace(workspaceName, { ignoreChanges: true });
   };
   const onFormSubmit = () => {
     props.state?.updateParent?.();
