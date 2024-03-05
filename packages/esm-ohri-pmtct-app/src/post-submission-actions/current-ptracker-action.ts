4cad7f2e1bc1e477bddc4deefd30d8266cccab55
Hero <hherobiam@gmail.com>
2024-03-05 14:56:13 +0200

    OHRI-2156:Remove error 400 on ohri-namibia-working (#1816)
    * OHRI-2156:Remove error 400 on ohri-namibia-working
    
    * fixed the build
    
    * update on the build fix
    
    * removed the unused import
    
    ---------
    
    Co-authored-by: Amos Laboso <amos.laboso@gmail.com>

--- packages/esm-ohri-pmtct-app/src/post-submission-actions/current-ptracker-action.ts
+++ packages/esm-ohri-pmtct-app/src/post-submission-actions/current-ptracker-action.ts
@@ -18,12 +18,16 @@ export const PTrackerSubmissionAction: PostSubmissionAction = {
 
 export async function updatePatientPtracker(encounter, encounterLocation, patientUuid) {
   const config = await getConfig('@ohri/openmrs-esm-ohri-pmtct');
-  const inComingPTrackerID = encounter.obs.find((observation) => observation.concept.uuid === config.obsConcepts.pTrackerIdConcept)?.value;
+  const inComingPTrackerID = encounter.obs.find(
+    (observation) => observation.concept.uuid === config.obsConcepts.pTrackerIdConcept,
+  )?.value;
   if (!inComingPTrackerID) {
     return;
   }
   const patientIdentifiers = await fetchPatientIdentifiers(patientUuid);
-  const existingPTrackers = patientIdentifiers.filter((id) => id.identifierType.uuid === config.encounterTypes.PTrackerIdentifierType);
+  const existingPTrackers = patientIdentifiers.filter(
+    (id) => id.identifierType.uuid === config.identifiersTypes.ptrackerIdentifierType,
+  );
   if (existingPTrackers.some((ptracker) => ptracker.identifier === inComingPTrackerID)) {
     return;
   }
@@ -31,7 +35,7 @@ export async function updatePatientPtracker(encounter, encounterLocation, patien
   //add current ptracker to identities
   const currentPTrackerObject: PatientIdentifier = {
     identifier: inComingPTrackerID,
-    identifierType: config.encounterTypes.PTrackerIdentifierType,
+    identifierType: config.identifiersTypes.ptrackerIdentifierType,
     location: encounterLocation,
     preferred: false,
   };
