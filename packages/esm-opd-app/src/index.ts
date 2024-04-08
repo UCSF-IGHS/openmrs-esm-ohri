97f893125a40ee3bc76c083e05d599bfa2e6272a
lucyjemutai <130601439+lucyjemutai@users.noreply.github.com>
2024-04-08 18:13:15 +0300

    opd updates (#1829)

--- packages/esm-opd-app/src/index.ts
+++ packages/esm-opd-app/src/index.ts
@@ -1,8 +1,7 @@
 import { defineConfigSchema, getAsyncLifecycle, getSyncLifecycle, registerFeatureFlag } from '@openmrs/esm-framework';
-import { opdFolderMeta } from './dashboard.meta';
+import { opdFolderMeta, activeVisitDashboardMeta } from './dashboard.meta';
 import { createDashboardGroup, createDashboardLink } from '@openmrs/esm-patient-common-lib';
 import { configSchema } from './config-schema';
-import { activeVisitDashboardMeta } from '@ohri/esm-patient-chart-app/src/dashboard.meta';
 
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
