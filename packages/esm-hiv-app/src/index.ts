778e988f0252934a1dd304f8aaf5a045d57380f2
CynthiaKamau <cynthiakamau54@gmail.com>
2024-05-21 19:57:17 +0300

    (chore) Removed unused imports and routes after homepage migration

--- packages/esm-hiv-app/src/index.ts
+++ packages/esm-hiv-app/src/index.ts
@@ -1,11 +1,5 @@
 import { defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
 import ServiceSummaryOverviewList from './views/service-summary/encounter-list/service-summary-encounter-list.component';
-import HTSSummaryTiles from './views/hts/home/summary-tiles/hts-summary-tiles.component';
-import OHRIPatientTabs from './views/hts/home/patient-tabs/ohri-patient-tabs.component';
-import CTSummaryTiles from './views/hts/care-and-treatment/summary-tiles/ct-summary-tiles.component';
-import CTHomePatientTabs from './views/hts/patient-list-tabs/ct-patient-list-tabs.component';
-import LabResultsSummaryTiles from './views/hts/lab-results/lab-results-summary-tiles.component';
-import LabResultsSummary from './views/hts/lab-results/lab-results-summary.component';
 import HTSPreventionSummary from './views/hiv-testing-services/hts-prevention-summary.component';
 import ProgramManagementSummary from './views/program-management/program-management-summary.component';
 import VisitsSummary from './views/visits/visits-summary.component';
@@ -14,16 +8,12 @@ import PartnerNotificationServices from './views/partner-notification-services/p
 import {
   createOHRIDashboardLink,
   PatientStatusBannerTag,
-  OHRIHome,
-  OHRIWelcomeSection,
   createOHRIGroupedLink,
 } from '@ohri/openmrs-esm-ohri-commons-lib';
 import { createDashboardGroup, createDashboardLink } from '@openmrs/esm-patient-common-lib';
 
 import {
   hivCareAndTreatmentFolderDashboardMeta,
-  careAndTreatmentDashboardMeta,
-  htsDashboardMeta,
   htsSummaryDashboardMeta,
   hivPreventionDashboardMeta,
   hivCareAndTreatmentDashboardDMeta,
@@ -61,36 +51,6 @@ export const htsServiceSummaryList = getSyncLifecycle(ServiceSummaryOverviewList
   moduleName,
 });
 
-export const htsHomeHeader = getSyncLifecycle(OHRIWelcomeSection, {
-  featureName: 'hts-home-header',
-  moduleName,
-});
-
-export const htsHomeTiles = getSyncLifecycle(HTSSummaryTiles, {
-  featureName: 'hts-home-tiles',
-  moduleName,
-});
-
-export const htsHomeTabs = getSyncLifecycle(OHRIPatientTabs, {
-  featureName: 'hts-home-tabs',
-  moduleName,
-});
-
-export const ctHomeHeader = getSyncLifecycle(OHRIWelcomeSection, {
-  featureName: 'ct-home-header',
-  moduleName,
-});
-
-export const ctHomeTiles = getSyncLifecycle(CTSummaryTiles, {
-  featureName: 'ct-home-tiles',
-  moduleName,
-});
-
-export const ctHomeTabs = getSyncLifecycle(CTHomePatientTabs, {
-  featureName: 'ct-home-tabs',
-  moduleName,
-});
-
 export const hivCareAndTreatmentFolderLink = getSyncLifecycle(
   createOHRIDashboardLink(hivCareAndTreatmentFolderDashboardMeta),
   options,
@@ -110,22 +70,6 @@ export const hivPreventionDashboardLink = getSyncLifecycle(
   options,
 );
 export const hivPreventionDashboard = getSyncLifecycle(htsRootComponent, options);
-
-// Lab Results
-export const labResultsHomeHeader = getSyncLifecycle(OHRIWelcomeSection, {
-  featureName: 'lab-results-home-header',
-  moduleName,
-});
-
-export const labResultsHomeTiles = getSyncLifecycle(LabResultsSummaryTiles, {
-  featureName: 'lab-results-home-tiles',
-  moduleName,
-});
-
-export const labResultsHomeTabs = getSyncLifecycle(LabResultsSummary, {
-  featureName: 'lab-results-tabs',
-  moduleName,
-});
 
 // Patient Chart
 export const patientChartHIVPreventionDashboard = getSyncLifecycle(
