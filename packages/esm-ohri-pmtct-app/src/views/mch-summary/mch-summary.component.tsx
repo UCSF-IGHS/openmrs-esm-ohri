b759a90c57c8aeb8266cbbdb9e92f7c17b574eb2
lucyjemutai <lucyjemutai.2019@gmail.com>
2024-05-23 16:24:31 +0300

    ptrackerId search

--- packages/esm-ohri-pmtct-app/src/views/mch-summary/mch-summary.component.tsx
+++ packages/esm-ohri-pmtct-app/src/views/mch-summary/mch-summary.component.tsx
@@ -18,10 +18,15 @@ const MaternalSummary: React.FC<PatientChartProps> = ({ patientUuid }) => {
 
   useEffect(() => {
     if (patient) {
-      const reversedIdentifiers = patient.identifier.slice().reverse();
-      const pTrackerIdentifier = reversedIdentifiers.find((identifier) => {
-        return identifier.type.coding[0].code === identifiersTypes.ptrackerIdentifierType;
-      });
+      const identifiers = patient.identifier;
+      let pTrackerIdentifier;
+
+      for (let i = identifiers.length - 1; i >= 0; i--) {
+        if (identifiers[i].type.coding[0].code === identifiersTypes.ptrackerIdentifierType) {
+          pTrackerIdentifier = identifiers[i];
+          break;
+        }
+      }
 
       if (pTrackerIdentifier) {
         setPtrackerId(pTrackerIdentifier.value);
