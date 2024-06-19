a796789cd33196b2efd1d31202867f46b811ec70
CynthiaKamau <cynthiakamau54@gmail.com>
2024-06-19 08:56:47 +0300

    (refactor) Remove redundant files in pmtct homepage

--- packages/esm-ohri-pmtct-app/src/home.component.tsx
+++ packages/esm-ohri-pmtct-app/src/home.component.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { OHRIWelcomeSection } from '@ohri/openmrs-esm-ohri-commons-lib';
 import MaternalChildSummaryTiles from './views/summary-tabs/maternal-child-summary-tiles.component';
-import LabResultsSummary from './views/summary-tabs/mother-child-summary-tabs.component';
+import MotherChildSummary from './views/summary-tabs/mother-child-summary-tabs.component';
 
 const Home: React.FC = () => {
   return (
     <div>
       <OHRIWelcomeSection title="Maternal & Child Health" />
       <MaternalChildSummaryTiles />
-      <LabResultsSummary />
+      <MotherChildSummary />
     </div>
   );
 };
