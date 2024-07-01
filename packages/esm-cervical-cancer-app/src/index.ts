d74f93dd57d50d657109ee921589606f600fbb04
hadijahkyampeire <hadijah315@gmail.com>
2024-07-02 02:23:15 +0300

    clean up cacx folders

--- packages/esm-cervical-cancer-app/src/index.ts
+++ packages/esm-cervical-cancer-app/src/index.ts
@@ -2,8 +2,8 @@ import { defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
 import { caCxSummaryDashboardMeta, caCxVisitsDashboardMeta, cervicalCancerFolderMeta } from './dashboard.meta';
 import { createDashboardLink } from '@openmrs/esm-patient-common-lib';
 import { createConditionalDashboardGroup } from '@ohri/openmrs-esm-ohri-commons-lib';
-import CacxSummaryList from './views/cacx-summary/cacx-summary.component';
-import CacxVisitServices from './views/cacx-visits/cacx-visits-services.component';
+import CacxSummaryList from './cervical-cancer/cacx-summary/cacx-summary.component';
+import CacxVisitServices from './cervical-cancer/cacx-visits/cacx-visits-services.component';
 import { configSchema } from './config-schema';
 
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
--- packages/esm-covid-app/src/index.ts
+++ packages/esm-covid-app/src/index.ts
@@ -1,7 +1,7 @@
 import { defineConfigSchema, getSyncLifecycle } from '@openmrs/esm-framework';
-import CovidVaccinations from './covid/patient chart/covid-vaccinations.component';
-import CovidLabResults from './covid/patient chart/lab-results.component';
-import CovidAssessment from './covid/patient chart/case-assessment.component';
+import CovidVaccinations from './covid/patient-chart/covid-vaccinations.component';
+import CovidLabResults from './covid/patient-chart/lab-results.component';
+import CovidAssessment from './covid/patient-chart/case-assessment.component';
 import {
   covidAssessmentsDashboardMeta,
   covidLabTestsDashboardMeta,
