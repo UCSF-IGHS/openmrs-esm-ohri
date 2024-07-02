6154c8494b189f7b07dda381acecde8adefdaacc
lucille <lucyjemutai.2019@gmail.com>
2024-07-02 14:59:39 +0300

    Move the tabs implementation in the commons-lib -cacx

--- packages/esm-cervical-cancer-app/src/cervical-cancer/cacx-visits/cacx-visits-services.component.tsx
+++ packages/esm-cervical-cancer-app/src/cervical-cancer/cacx-visits/cacx-visits-services.component.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Tabs, Tab, TabList, TabPanels, TabPanel } from '@carbon/react';
 import { useConfig } from '@openmrs/esm-framework';
-import { EncounterList, getMenuItemTabConfiguration } from '@ohri/openmrs-esm-ohri-commons-lib';
+import { getMenuItemTabConfiguration, TabsComponent } from '@ohri/openmrs-esm-ohri-commons-lib';
+// import TabsComponent from '../../../../esm-commons-lib/src/components/encounter-tabs/encounter-tabs.component';
 import cacxConfigSchema from './cacx-config.json';
-
-import styles from '../common.scss';
 
 interface OverviewListProps {
   patientUuid: string;
@@ -14,32 +12,7 @@ const CaCxCervicalCancerServices: React.FC<OverviewListProps> = ({ patientUuid }
   const config = useConfig();
   const tabs = getMenuItemTabConfiguration(cacxConfigSchema, config);
 
-  return (
-    <div className={styles.tabContainer}>
-      <Tabs>
-        <TabList contained>
-          {tabs.map((tab) => (
-            <Tab key={tab.name}>{tab.name}</Tab>
-          ))}
-        </TabList>
-        <TabPanels>
-          {tabs.map((tab) => (
-            <TabPanel>
-              <EncounterList
-                patientUuid={patientUuid}
-                formList={tab.formList}
-                columns={tab.columns}
-                encounterType={tab.encounterType}
-                launchOptions={tab.launchOptions}
-                headerTitle={tab.headerTitle}
-                description={tab.description}
-              />
-            </TabPanel>
-          ))}
-        </TabPanels>
-      </Tabs>
-    </div>
-  );
+  return <TabsComponent patientUuid={patientUuid} tabsConfig={tabs} />;
 };
 
 export default CaCxCervicalCancerServices;
---
+++ packages/esm-commons-lib/src/components/encounter-tabs/encounter-tabs.component.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Tabs, Tab, TabList, TabPanels, TabPanel } from '@carbon/react';
+import { EncounterList } from '@ohri/openmrs-esm-ohri-commons-lib';
+import styles from '../common.scss';
+
+interface OverviewListProps {
+  patientUuid: string;
+  tabsConfig: any[];
+}
+
+const TabsComponent = ({ patientUuid, tabsConfig }: OverviewListProps) => {
+  return (
+    <div className={styles.tabContainer}>
+      <Tabs>
+        <TabList contained>
+          {tabsConfig.map((tab) => (
+            <Tab key={tab.name}>{tab.name}</Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {tabsConfig.map((tab) => (
+            <TabPanel key={tab.name}>
+              <EncounterList
+                patientUuid={patientUuid}
+                formList={tab.formList}
+                columns={tab.columns}
+                encounterType={tab.encounterType}
+                launchOptions={tab.launchOptions}
+                headerTitle={tab.headerTitle}
+                description={tab.description}
+              />
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
+};
+
+export default TabsComponent;
