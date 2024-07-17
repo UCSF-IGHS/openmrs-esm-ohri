5aa7058943d0a380a4b03a808122bc7bca03fa17
lucille <lucyjemutai.2019@gmail.com>
2024-07-17 16:20:25 +0300

    c

---
+++ packages/esm-ptracker-report-app/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const config = (module.exports = require('openmrs/default-webpack-config'));
+config.scriptRuleConfig.exclude =
+  path.sep == '/'
+    ? /(node_modules[^\/@openmrs\/esm\-patient\-common\-lib])/
+    : /(node_modules[^\\@openmrs\/esm\-patient\-common\-lib])/;
+config.overrides.resolve = {
+  extensions: ['.tsx', '.ts', '.jsx', '.js', '.scss'],
+  alias: {
+    '@openmrs/esm-framework': '@openmrs/esm-framework/src/internal',
+    '@ohri/openmrs-esm-ohri-commons-lib': path.resolve(__dirname, '../esm-commons-lib/src/index'),
+    '@openmrs/openmrs-form-engine-lib': '@openmrs/openmrs-form-engine-lib/src/index',
+  },
+};
+module.exports = config;
